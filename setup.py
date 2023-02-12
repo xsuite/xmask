60a5ed674067e13de09c27a198eb579a10534e18
giadarol <giovanni.iadarola@cern.ch>
2023-02-12 06:57:31 +0100

    Adapted setup.py

--- setup.py
+++ setup.py
@@ -16,37 +16,33 @@ extensions = []
 # Setup #
 #########
 
-version_file = Path(__file__).parent / 'xtrack/_version.py'
+version_file = Path(__file__).parent / 'pymaskmx/_version.py'
 dd = {}
 with open(version_file.absolute(), 'r') as fp:
     exec(fp.read(), dd)
 __version__ = dd['__version__']
 
 setup(
-    name='xtrack',
+    name='pymaskmx',
     version=__version__,
-    description='Tracking library for particle accelerators',
-    long_description='Tracking library for particle accelerators',
-    url='https://xsuite.readthedocs.io/',
+    description='Configuration of tracking simulations for the LHC and other accelerators',
+    long_description='Configuration of tracking simulations for the LHC and other accelerator',
+    url='https://github.com/xsuite/pymaskmx/issues',
     author='G. Iadarola et al.',
     license='Apache 2.0',
-    download_url="https://pypi.python.org/pypi/xtrack",
+    download_url="https://pypi.python.org/pypi/pymaskmx",
     project_urls={
-            "Bug Tracker": "https://github.com/xsuite/xsuite/issues",
-            "Documentation": 'https://xsuite.readthedocs.io/',
-            "Source Code": "https://github.com/xsuite/xtrack",
+            "Bug Tracker": "https://github.com/xsuite/pymaskmx/issues",
+            "Source Code": "https://github.com/pymaskmx/xtrack",
         },
     packages=find_packages(),
     ext_modules = extensions,
     include_package_data=True,
     install_requires=[
         'numpy>=1.0',
-        'scipy',
-        'xobjects',
-        'xpart',
-        'xdeps'
+        'xsuite',
         ],
     extras_require={
-        'tests': ['cpymad', 'PyHEADTAIL', 'pytest', 'pytest-mock'],
+        'tests': ['cpymad', 'pytest'],
         },
     )
