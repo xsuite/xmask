947e373a05e9e7af90b991e3db9d7523264d62f7
giadarol <giovanni.iadarola@cern.ch>
2023-03-07 08:48:49 +0100

    Start parametrizing

--- examples/hllhc14_collision/004_check_bb_one_by_one.py
+++ examples/hllhc14_collision/004_check_bb_one_by_one.py
@@ -1,9 +1,10 @@
 import json
-import xobjects as xo
-import xtrack as xt
-import xfields as xf
 import numpy as np
 from scipy.stats import norm
+import pandas as pd
+
+import xtrack as xt
+import xfields as xf
 
 # Reference constant charge slicing
 # from  https://github.com/giadarol/WeakStrong/blob/master/slicing.py
@@ -20,15 +21,18 @@ def _get_z_centroids(ho_slices, sigmaz):
             ) * ho_slices)
     return np.array(z_centroids + [0] + [-ii for ii in z_centroids[-1::-1]])
 
-# Import the weak and strong lines
-verbose = True
-with open('collider_02_bb_on.json', 'r') as fid:
-    collider = xt.Multiline.from_dict(json.load(fid))
+ip_bb_config= {
+    'ip1': {'num_lr_per_side': 25},
+    'ip2': {'num_lr_per_side': 20},
+    'ip5': {'num_lr_per_side': 25},
+    'ip8': {'num_lr_per_side': 20},
+}
 
-collider.build_trackers()
+line_config = {
+    'lhcb1': {'strong_beam': 'lhcb2', 'sorting': {'l': -1, 'r': 1}},
+    'lhcb2': {'strong_beam': 'lhcb1', 'sorting': {'l': 1, 'r': -1}},
+}
 
-bb_ip_n_list = [1, 2, 5, 8]
-num_long_range_encounters_per_side = [25, 20, 25, 20]
 nemitt_x = 2e-6
 nemitt_y = 3e-6
 harmonic_number = 35640
@@ -36,16 +40,70 @@ bunch_spacing_buckets = 10
 sigmaz = 0.076
 num_slices_head_on = 11
 num_particles = 2.2e11
+qx_no_bb = 62.31
+qy_no_bb = 60.32
+
+# Import the weak and strong lines
+verbose = True
+with open('collider_02_bb_on.json', 'r') as fid:
+    collider = xt.Multiline.from_dict(json.load(fid))
+
+collider.build_trackers()
 
 
+for line_name in ['lhcb1', 'lhcb2']:
 
-ip = 1 # will be parametrized by pytest
-num_lr_per_side = 25 # will be parametrized by pytest
+    print(f'Global check on line {line_name}')
+
+    # Check that the number of lenses is correct
+    df = collider[line_name].to_pandas()
+    bblr_df = df[df['element_type'] == 'BeamBeamBiGaussian2D']
+    bbho_df = df[df['element_type'] == 'BeamBeamBiGaussian3D']
+    bb_df = pd.concat([bblr_df, bbho_df])
+
+    assert (len(bblr_df) == 2 * sum(
+        [ip_bb_config[ip]['num_lr_per_side'] for ip in ip_bb_config.keys()]))
+    assert (len(bbho_df) == len(ip_bb_config.keys()) * num_slices_head_on)
+
+    # Check that beam-beam scale knob works correctly
+    collider.vars['beambeam_scale'] = 1
+    for nn in bb_df.name.values:
+        assert collider[line_name][nn].scale_strength == 1
+    collider.vars['beambeam_scale'] = 0
+    for nn in bb_df.name.values:
+        assert collider[line_name][nn].scale_strength == 0
+    collider.vars['beambeam_scale'] = 1
+    for nn in bb_df.name.values:
+        assert collider[line_name][nn].scale_strength == 1
+
+    # Twiss with and without bb
+    collider.vars['beambeam_scale'] = 1
+    tw_bb_on = collider[line_name].twiss()
+    collider.vars['beambeam_scale'] = 0
+    tw_bb_off = collider[line_name].twiss()
+    collider.vars['beambeam_scale'] = 1
+
+    assert np.isclose(tw_bb_off.qx, qx_no_bb, rtol=0, atol=1e-5)
+    assert np.isclose(tw_bb_off.qy, qy_no_bb, rtol=0, atol=1e-5)
+
+    # Check that there is a tune shift of the order of 1.5e-2
+    assert np.isclose(tw_bb_on.qx, qx_no_bb - 1.5e-2, rtol=0, atol=4e-3)
+    assert np.isclose(tw_bb_on.qy, qy_no_bb - 1.5e-2, rtol=0, atol=4e-3)
+
+    # Check that there is no effect on the orbit
+    np.allclose(tw_bb_on.x, tw_bb_off.x, atol=1e-10, rtol=0)
+    np.allclose(tw_bb_on.y, tw_bb_off.y, atol=1e-10, rtol=0)
+
+
 name_weak = 'lhcb1' # will be parametrized by pytest
-name_strong = 'lhcb2' # will be parametrized by pytest
-sorting = {'l': -1 , 'r': 1} # will be parametrized by pytest
+ip = 'ip1'
 
-# ip = 5 # will be parametrized by pytest
+ip_n = int(ip[2])
+num_lr_per_side = ip_bb_config[ip]['num_lr_per_side']
+name_strong = line_config[name_weak]['strong_beam']
+sorting = line_config[name_weak]['sorting']
+
+# ip_n = 5 # will be parametrized by pytest
 # num_lr_per_side = 25 # will be parametrized by pytest
 # name_weak = 'lhcb2' # will be parametrized by pytest
 # name_strong = 'lhcb1' # will be parametrized by pytest
@@ -59,19 +117,18 @@ with xt.tracker._temp_knobs(collider, knobs={'beambeam_scale': 0}):
 
 # Survey starting from ip
 print('Survey(s) (starting from ip)')
-survey_weak = collider[name_weak].survey(element0=f'ip{ip}')
-survey_strong = collider[name_strong].survey(element0=f'ip{ip}', reverse=True)
+survey_weak = collider[name_weak].survey(element0=f'ip{ip_n}')
+survey_strong = collider[name_strong].survey(element0=f'ip{ip_n}', reverse=True)
 beta0_strong = collider[name_strong].particle_ref.beta0[0]
 gamma0_strong = collider[name_strong].particle_ref.gamma0[0]
-
 
 bunch_spacing_ds = tw_weak.circumference/harmonic_number*bunch_spacing_buckets
 
 # Check lr encounters
 for side in ['l', 'r']:
     for iele in range(num_lr_per_side):
-        nn_weak = f'bb_lr.{side}{ip}b{name_weak[-1]}_{iele+1:02d}'
-        nn_strong = f'bb_lr.{side}{ip}b{name_strong[-1]}_{iele+1:02d}'
+        nn_weak = f'bb_lr.{side}{ip_n}b{name_weak[-1]}_{iele+1:02d}'
+        nn_strong = f'bb_lr.{side}{ip_n}b{name_strong[-1]}_{iele+1:02d}'
 
         assert nn_weak in tw_weak.name
         assert nn_strong in tw_strong.name
@@ -91,6 +148,9 @@ for side in ['l', 'r']:
         assert np.isclose(ee_weak.other_beam_Sigma_33, expected_sigma_y**2,
                           atol=0, rtol=1e-6)
 
+        # Check no coupling
+        assert ee_weak.other_beam_Sigma_13 == 0
+
         # Orbit
         assert np.isclose(ee_weak.ref_shift_x, tw_weak[nn_weak, 'x'],
                           rtol=0, atol=1e-4 * expected_sigma_x)
@@ -109,10 +169,20 @@ for side in ['l', 'r']:
                             rtol=0, atol=1e-4 * expected_sigma_y)
 
         # s position
-        assert np.isclose(tw_weak[nn_weak, 's'] - tw_weak[f'ip{ip}', 's'],
+        assert np.isclose(tw_weak[nn_weak, 's'] - tw_weak[f'ip{ip_n}', 's'],
                           bunch_spacing_ds/2 * (iele+1) * sorting[side],
                           rtol=0, atol=10e-6)
 
+        # Check intensity
+        assert np.isclose(ee_weak.other_beam_num_particles, num_particles,
+                          atol=0, rtol=1e-8)
+
+        # Other checks
+        assert ee_weak.min_sigma_diff < 1e-9
+        assert ee_weak.min_sigma_diff > 0
+
+        assert ee_weak.scale_strength == 1
+        assert ee_weak.other_beam_q0 == 1
 
 # Check head on encounters
 
@@ -133,8 +203,8 @@ with xt.tracker._temp_knobs(collider, knobs={'beambeam_scale': 0}):
                                 freeze_longitudinal=True, reverse=True)
     tw_z_crab_minus = collider[name_strong].twiss(zeta0=-z_crab_test, method='4d',
                                 freeze_longitudinal=True, reverse=True)
-phi_crab_x = -(tw_z_crab_plus[f'ip{ip}', 'x'] - tw_z_crab_minus[f'ip{ip}', 'x']) / (2*z_crab_test)
-phi_crab_y = -(tw_z_crab_plus[f'ip{ip}', 'y'] - tw_z_crab_minus[f'ip{ip}', 'y']) / (2*z_crab_test)
+phi_crab_x = -(tw_z_crab_plus[f'ip{ip_n}', 'x'] - tw_z_crab_minus[f'ip{ip_n}', 'x']) / (2*z_crab_test)
+phi_crab_y = -(tw_z_crab_plus[f'ip{ip_n}', 'y'] - tw_z_crab_minus[f'ip{ip_n}', 'y']) / (2*z_crab_test)
 
 for ii, zz in list(zip(range(-(num_slices_head_on - 1) // 2,
                        (num_slices_head_on - 1) // 2 + 1),
@@ -147,8 +217,8 @@ for ii, zz in list(zip(range(-(num_slices_head_on - 1) // 2,
     else:
         side = 'r' if sorting['r'] == 1 else 'l'
 
-    nn_weak = f'bb_ho.{side}{ip}b{name_weak[-1]}_{int(abs(ii)):02d}'
-    nn_strong = f'bb_ho.{side}{ip}b{name_strong[-1]}_{int(abs(ii)):02d}'
+    nn_weak = f'bb_ho.{side}{ip_n}b{name_weak[-1]}_{int(abs(ii)):02d}'
+    nn_strong = f'bb_ho.{side}{ip_n}b{name_strong[-1]}_{int(abs(ii)):02d}'
 
     ee_weak = collider[name_weak][nn_weak]
 
@@ -158,7 +228,7 @@ for ii, zz in list(zip(range(-(num_slices_head_on - 1) // 2,
 
     # s position
     expected_s = zz / 2
-    assert np.isclose(tw_weak[nn_weak, 's'] - tw_weak[f'ip{ip}', 's'],
+    assert np.isclose(tw_weak[nn_weak, 's'] - tw_weak[f'ip{ip_n}', 's'],
                       expected_s, atol=10e-6, rtol=0)
 
     # Beam sizes
@@ -234,16 +304,16 @@ for ii, zz in list(zip(range(-(num_slices_head_on - 1) // 2,
 
     # Check crossing angle
     # Assume that crossing is either in x or in y
-    if tw_weak[f'ip{ip}', 'px'] < 1e-6:
+    if tw_weak[f'ip{ip_n}', 'px'] < 1e-6:
         # Vertical crossing
         assert np.isclose(ee_weak.alpha, np.pi/2, atol=1e-3, rtol=0)
         assert np.isclose(
-            2*ee_weak.phi, tw_weak[f'ip{ip}', 'py'] - tw_strong[f'ip{ip}', 'py'],
+            2*ee_weak.phi, tw_weak[f'ip{ip_n}', 'py'] - tw_strong[f'ip{ip_n}', 'py'],
             atol=1e-7, rtol=0)
     else:
         assert np.isclose(ee_weak.alpha, 0, atol=1e-3, rtol=0)
         assert np.isclose(
-            2*ee_weak.phi, tw_weak[f'ip{ip}', 'px'] - tw_strong[f'ip{ip}', 'px'],
+            2*ee_weak.phi, tw_weak[f'ip{ip_n}', 'px'] - tw_strong[f'ip{ip_n}', 'px'],
             atol=1e-7, rtol=0)
 
     # Check intensity
