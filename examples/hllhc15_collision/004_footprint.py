002fb3734b3394af3e7da59b18cc8101b86fdd35
giadarol <giovanni.iadarola@cern.ch>
2023-03-22 17:39:02 +0100

    Working on example

--- examples/hllhc15_collision/004_footprint.py
+++ examples/hllhc15_collision/004_footprint.py
@@ -13,20 +13,20 @@ collider.build_trackers()
 #                                      n_fft=2**18)
 
 
-# collider.vars['beambeam_scale'] = 0.1
-
-# fp1 = collider['lhcb1'].get_footprint(nemitt_x=2.5e-6, nemitt_y=2.5e-6,
-#                                      theta_range=(0.05, np.pi/2-0.05),
-#                                      n_fft=2**18)
-
-# qx = fp0.qx + (fp1.qx - fp0.qx)/0.1*1.
-# qy = fp0.qy + (fp1.qy - fp0.qy)/0.1*1.
 
 from xtrack.footprint import _footprint_with_linear_rescale, LinearRescale
 
 collider.vars['beambeam_scale'] = 1
-fp = collider['lhcb1'].get_footprint(
+fp_ua = collider['lhcb1'].get_footprint(
     nemitt_x=2.5e-6, nemitt_y=2.5e-6,
+    mode='uniform_action_grid',
+    linear_rescale_on_knobs=[
+        LinearRescale(knob_name='beambeam_scale', v0=0.0, dv=0.1)]
+    )
+
+fp_polar = collider['lhcb1'].get_footprint(
+    nemitt_x=2.5e-6, nemitt_y=2.5e-6,
+    mode='uniform_action_grid',
     linear_rescale_on_knobs=[
         LinearRescale(knob_name='beambeam_scale', v0=0.0, dv=0.1)]
     )
