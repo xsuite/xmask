674035ff978e730983f7387591c7825bd766a0b9
giadarol <giovanni.iadarola@cern.ch>
2023-03-22 17:15:54 +0100

    Linear rescale works

--- examples/hllhc15_collision/004_footprint.py
+++ examples/hllhc15_collision/004_footprint.py
@@ -6,33 +6,30 @@ import xtrack as xt
 collider = xt.Multiline.from_json('./collider_03_tuned_bb_on.json')
 collider.build_trackers()
 
-collider.vars['beambeam_scale'] = 0.3
+# collider.vars['beambeam_scale'] = 0.0
 
-fp = collider['lhcb1'].get_footprint(nemitt_x=2.5e-6, nemitt_y=2.5e-6,
-                                     theta_range=(0.05, np.pi/2-0.05),
-                                     n_fft=2**18)
-# Find problematic points
-
-i_problems = np.where(np.abs(fp.qx.flatten() - fp.qy.flatten()) < 1e-4)
-
-fftp_x = np.squeeze(fp.fft_x[98, :])
-fftp_y = np.squeeze(fp.fft_y[98, :])
+# fp0 = collider['lhcb1'].get_footprint(nemitt_x=2.5e-6, nemitt_y=2.5e-6,
+#                                      theta_range=(0.05, np.pi/2-0.05),
+#                                      n_fft=2**18)
 
 
-fftp_y_scaled = np.abs(fftp_y) / np.max(np.abs(fftp_y)) * np.max(np.abs(fftp_x))
+# collider.vars['beambeam_scale'] = 0.1
 
-import matplotlib.pyplot as plt
-plt.close('all')
-plt.figure(1)
-freq = np.fft.rfftfreq(len(fftp_x)*2 - 1)
-plt.plot(freq, np.abs(fftp_x))
-plt.plot(freq, fftp_y_scaled)
-plt.plot(freq, np.abs(fftp_x) - np.abs(fftp_y_scaled))
+# fp1 = collider['lhcb1'].get_footprint(nemitt_x=2.5e-6, nemitt_y=2.5e-6,
+#                                      theta_range=(0.05, np.pi/2-0.05),
+#                                      n_fft=2**18)
 
-plt.show()
+# qx = fp0.qx + (fp1.qx - fp0.qx)/0.1*1.
+# qy = fp0.qy + (fp1.qy - fp0.qy)/0.1*1.
+
+from xtrack.footprint import _footprint_with_linear_rescale, LinearRescale
+
+collider.vars['beambeam_scale'] = 1
+fp_lr  = _footprint_with_linear_rescale(line= collider['lhcb1'],
+        kwargs={'nemitt_x': 2.5e-6, 'nemitt_y': 2.5e-6,
+                'theta_range': (0.05, np.pi/2-0.05),
+                'n_fft': 2**18},
+        linear_rescale_on_knobs=[
+            LinearRescale(knob_name='beambeam_scale', v0=0.0, dv=0.1)])
 
 
-# fpj = collider['lhcb1'].get_footprint(nemitt_x=1e-6, nemitt_y=1e-6,
-#                                       mode='uniform_action_grid',
-#                                       x_norm_range=(0.1, 6),
-#                                       y_norm_range=(0.1, 6))
