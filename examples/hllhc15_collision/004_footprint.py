0fa9c3e091b98b8774006dc336fe9561ad093d24
giadarol <giovanni.iadarola@cern.ch>
2023-03-22 17:30:18 +0100

    A clean footprint

--- examples/hllhc15_collision/004_footprint.py
+++ examples/hllhc15_collision/004_footprint.py
@@ -25,11 +25,10 @@ collider.build_trackers()
 from xtrack.footprint import _footprint_with_linear_rescale, LinearRescale
 
 collider.vars['beambeam_scale'] = 1
-fp_lr  = _footprint_with_linear_rescale(line= collider['lhcb1'],
-        kwargs={'nemitt_x': 2.5e-6, 'nemitt_y': 2.5e-6,
-                'theta_range': (0.05, np.pi/2-0.05),
-                'n_fft': 2**18},
-        linear_rescale_on_knobs=[
-            LinearRescale(knob_name='beambeam_scale', v0=0.0, dv=0.1)])
+fp = collider['lhcb1'].get_footprint(
+    nemitt_x=2.5e-6, nemitt_y=2.5e-6,
+    linear_rescale_on_knobs=[
+        LinearRescale(knob_name='beambeam_scale', v0=0.0, dv=0.1)]
+    )
 
 
