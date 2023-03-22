8de8c0bc7357cc1aa1474beaecd826c9b5b8f1bf
giadarol <giovanni.iadarola@cern.ch>
2023-03-22 17:44:05 +0100

    Plots in example

--- examples/hllhc15_collision/004_footprint.py
+++ examples/hllhc15_collision/004_footprint.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 import xtrack as xt
 
@@ -6,29 +7,29 @@ import xtrack as xt
 collider = xt.Multiline.from_json('./collider_03_tuned_bb_on.json')
 collider.build_trackers()
 
-# collider.vars['beambeam_scale'] = 0.0
-
-# fp0 = collider['lhcb1'].get_footprint(nemitt_x=2.5e-6, nemitt_y=2.5e-6,
-#                                      theta_range=(0.05, np.pi/2-0.05),
-#                                      n_fft=2**18)
-
-
-
-from xtrack.footprint import _footprint_with_linear_rescale, LinearRescale
 
 collider.vars['beambeam_scale'] = 1
 fp_ua = collider['lhcb1'].get_footprint(
     nemitt_x=2.5e-6, nemitt_y=2.5e-6,
     mode='uniform_action_grid',
     linear_rescale_on_knobs=[
-        LinearRescale(knob_name='beambeam_scale', v0=0.0, dv=0.1)]
+        xt.LinearRescale(knob_name='beambeam_scale', v0=0.0, dv=0.1)]
     )
 
 fp_polar = collider['lhcb1'].get_footprint(
     nemitt_x=2.5e-6, nemitt_y=2.5e-6,
-    mode='uniform_action_grid',
     linear_rescale_on_knobs=[
-        LinearRescale(knob_name='beambeam_scale', v0=0.0, dv=0.1)]
+        xt.LinearRescale(knob_name='beambeam_scale', v0=0.0, dv=0.1)]
     )
 
+plt.close('all')
 
+plt.figure(1)
+fp_polar.plot()
+plt.suptitle('Polar mode (default) - linear rescale on beambeam_scale')
+
+plt.figure(2)
+fp_ua.plot()
+plt.suptitle('Uniform action grid mode - linear rescale on beambeam_scale')
+
+plt.show()
