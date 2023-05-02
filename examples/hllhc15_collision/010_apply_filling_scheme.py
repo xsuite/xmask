04027575c36933beef488e6f0cb5116db779a73b
giadarol <giovanni.iadarola@cern.ch>
2023-05-02 21:01:24 +0200

    Move delay calculation from script to xfields

--- examples/hllhc15_collision/010_apply_filling_scheme.py
+++ examples/hllhc15_collision/010_apply_filling_scheme.py
@@ -8,45 +8,10 @@ collider.build_trackers()
 filling_pattern_cw = np.zeros(3564, dtype=int)
 filling_pattern_acw = np.zeros(3564, dtype=int)
 
-harmonic_number = 35640
-bunch_spacing_buckets = 10
+# harmonic_number = 35640
+# bunch_spacing_buckets = 10
 
-ip_names = collider._bb_config['ip_names'] # is ['ip1', 'ip2', 'ip5', 'ip8']
-delay_at_ips_slots = [0, 891, 0, 2670] # Defined as anticlockwise bunch id that
-                                       # meets bunch 0 of the clockwise beam
-
-ring_length_in_slots = harmonic_number / bunch_spacing_buckets
-
-for orientation in ['clockwise', 'anticlockwise']:
-
-    if orientation == 'clockwise':
-        delay_at_ips_dict = {iipp: dd
-                             for iipp, dd in zip(ip_names, delay_at_ips_slots)}
-    elif orientation == 'anticlockwise':
-        delay_at_ips_dict = {iipp: np.mod(ring_length_in_slots - dd, ring_length_in_slots)
-                             for iipp, dd in zip(ip_names, delay_at_ips_slots)}
-    else:
-        raise ValueError('?!')
-
-    bbdf = collider._bb_config['dataframes'][orientation]
-
-    delay_in_slots = []
-
-    for nn in bbdf.index.values:
-        ip_name = bbdf.loc[nn, 'ip_name']
-        this_delay = delay_at_ips_dict[ip_name]
-
-        if nn.startswith('bb_lr.'):
-            if orientation == 'clockwise':
-                this_delay += bbdf.loc[nn, 'identifier']
-            elif orientation == 'anticlockwise':
-                this_delay -= bbdf.loc[nn, 'identifier']
-            else:
-                raise ValueError('?!')
-
-        delay_in_slots.append(int(this_delay))
-
-    bbdf['delay_in_slots'] = delay_in_slots
+# ip_names = collider._bb_config['ip_names'] # is ['ip1', 'ip2', 'ip5', 'ip8']
 
 
 # Some checks
