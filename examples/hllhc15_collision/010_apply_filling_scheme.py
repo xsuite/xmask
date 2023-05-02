dc1382083d5b8d71ece45808d80452d50152ccb8
giadarol <giovanni.iadarola@cern.ch>
2023-05-02 13:58:06 +0200

    Start working on filling scheme

--- examples/hllhc15_collision/010_apply_filling_scheme.py
+++ examples/hllhc15_collision/010_apply_filling_scheme.py
@@ -11,3 +11,27 @@ filling_pattern_b2 = np.zeros(3564, dtype=int)
 # Fill 50 bunches around bunch 500
 filling_pattern_b1[500-25:500+25] = 1
 
+
+ip_names = collider._bb_config['ip_names'] # is ['ip1', 'ip2', 'ip5', 'ip8']
+delay_at_ips_slots = [0, 891, 0, 2670]
+
+delay_at_ips_dict = {iipp: dd for iipp, dd in zip(ip_names, delay_at_ips_slots)}
+
+# Start working on B1
+bbdf = collider._bb_config['dataframes']['clockwise']
+
+delay_in_slots = []
+
+for nn in bbdf.index.values:
+    ip_name = bbdf.loc[nn, 'ip_name']
+    this_delay = delay_at_ips_dict[ip_name]
+
+    if nn.startswith('bb_lr.'):
+        this_delay += bbdf.loc[nn, 'identifier']
+
+    delay_in_slots.append(this_delay)
+
+bbdf['delay_in_slots'] = delay_in_slots
+
+
+
