ada41f06c46375c51e3a59d32997a59675d22c91
giadarol <giovanni.iadarola@cern.ch>
2023-05-02 14:23:08 +0200

    Introduce info kin dframes

--- examples/hllhc15_collision/010_apply_filling_scheme.py
+++ examples/hllhc15_collision/010_apply_filling_scheme.py
@@ -10,28 +10,48 @@ filling_pattern_b2 = np.zeros(3564, dtype=int)
 
 # Fill 50 bunches around bunch 500
 filling_pattern_b1[500-25:500+25] = 1
+filling_pattern_b2[500-25:500+25] = 1
 
+
+harmonic_number = 35640
+bunch_spacing_buckets = 10
 
 ip_names = collider._bb_config['ip_names'] # is ['ip1', 'ip2', 'ip5', 'ip8']
-delay_at_ips_slots = [0, 891, 0, 2670]
+delay_at_ips_slots = [0, 891, 0, 2670] # Defined as anticlockwise bunch id that
+                                       # meets bunch 0 of the clockwise beam
 
-delay_at_ips_dict = {iipp: dd for iipp, dd in zip(ip_names, delay_at_ips_slots)}
+ring_length_in_slots = harmonic_number / bunch_spacing_buckets
 
-# Start working on B1
-bbdf = collider._bb_config['dataframes']['clockwise']
+for orientation in ['clockwise', 'anticlockwise']:
 
-delay_in_slots = []
+    if orientation == 'clockwise':
+        delay_at_ips_dict = {iipp: dd
+                             for iipp, dd in zip(ip_names, delay_at_ips_slots)}
+    elif orientation == 'anticlockwise':
+        delay_at_ips_dict = {iipp: ring_length_in_slots - dd
+                             for iipp, dd in zip(ip_names, delay_at_ips_slots)}
+    else:
+        raise ValueError('?!')
 
-for nn in bbdf.index.values:
-    ip_name = bbdf.loc[nn, 'ip_name']
-    this_delay = delay_at_ips_dict[ip_name]
+    bbdf = collider._bb_config['dataframes'][orientation]
 
-    if nn.startswith('bb_lr.'):
-        this_delay += bbdf.loc[nn, 'identifier']
+    delay_in_slots = []
 
-    delay_in_slots.append(this_delay)
+    for nn in bbdf.index.values:
+        ip_name = bbdf.loc[nn, 'ip_name']
+        this_delay = delay_at_ips_dict[ip_name]
 
-bbdf['delay_in_slots'] = delay_in_slots
+        if nn.startswith('bb_lr.'):
+            if orientation == 'clockwise':
+                this_delay += bbdf.loc[nn, 'identifier']
+            elif orientation == 'anticlockwise':
+                this_delay -= bbdf.loc[nn, 'identifier']
+            else:
+                raise ValueError('?!')
+
+        delay_in_slots.append(this_delay)
+
+    bbdf['delay_in_slots'] = delay_in_slots
 
 
 
