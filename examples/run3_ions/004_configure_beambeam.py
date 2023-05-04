576767cdcf445d6f6d3b9f5c40f4e7851d946e36
giadarol <giovanni.iadarola@cern.ch>
2023-05-04 20:42:54 +0200

    Homogenize run3_ions and hllhc15

--- examples/run3_ions/004_configure_beambeam.py
+++ examples/run3_ions/004_configure_beambeam.py
@@ -19,12 +19,16 @@ collider.configure_beambeam_interactions(
     nemitt_x=config_bb['nemitt_x'],
     nemitt_y=config_bb['nemitt_y'])
 
-with open('filling.json', 'r') as fid:
-    filling = json.load(fid)
+if 'mask_with_filling_pattern' in config_bb:
+    fname = config_bb['mask_with_filling_pattern']['pattern_fname']
+    i_bunch_cw = config_bb['mask_with_filling_pattern']['i_bunch_b1']
+    i_bunch_acw = config_bb['mask_with_filling_pattern']['i_bunch_b2']
+    with open(fname, 'r') as fid:
+        filling = json.load(fid)
 
-collider.apply_filling_pattern(
-    filling_pattern_cw=filling['beam1'],
-    filling_pattern_acw=filling['beam2'],
-    i_bunch_cw=488, i_bunch_acw=488)
+    collider.apply_filling_pattern(
+        filling_pattern_cw=filling['beam1'],
+        filling_pattern_acw=filling['beam2'],
+        i_bunch_cw=i_bunch_cw, i_bunch_acw=i_bunch_acw)
 
 collider.to_json('collider_04_tuned_and_leveled_bb_on.json')
