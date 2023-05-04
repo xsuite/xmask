3ff20c3da0ee06d162ecb447b1edaa383e1dac4a
giadarol <giovanni.iadarola@cern.ch>
2023-05-04 08:25:19 +0200

    Leveling fully parametrized

--- examples/hllhc15_collision/003_lumi_level_ip2_ip8.py
+++ examples/hllhc15_collision/003_lumi_level_ip2_ip8.py
@@ -14,96 +14,70 @@ with open('config.yaml','r') as fid:
 config_lumi_leveling_ip2_ip8 = config['config_lumi_leveling_ip2_ip8']
 config_beambeam = config['config_beambeam']
 
-# Leveling in IP2
-print('\n --- Leveling in IP2 ---')
-config_ip2 = config_lumi_leveling_ip2_ip8['ip2']
-collider.match(
-    lines=['lhcb1', 'lhcb2'],
-    ele_start=['e.ds.l2.b1', 's.ds.r2.b2'],
-    ele_stop=['s.ds.r2.b1', 'e.ds.l2.b2'],
-    twiss_init='preserve',
-    targets=[
-        xt.TargetSeparation(
-            ip_name='ip2', separation_norm=config_ip2['separation_in_sigmas'],
-            tol=1e-4, # in sigmas
-            plane=config_ip2['plane'],
-            nemitt_x=config_beambeam['nemitt_x'],
-            nemitt_y=config_beambeam['nemitt_y']),
-        # Preserve crossing angle
-        xt.TargetList(['px', 'py'], at='ip2', line='lhcb1', value='preserve', tol=1e-7, scale=1e3),
-        xt.TargetList(['px', 'py'], at='ip2', line='lhcb2', value='preserve', tol=1e-7, scale=1e3),
-        # Close the bumps
-        xt.TargetList(['x', 'y'], at='s.ds.r2.b1', line='lhcb1', value='preserve', tol=1e-5, scale=1),
-        xt.TargetList(['px', 'py'], at='s.ds.r2.b1', line='lhcb1', value='preserve', tol=1e-5, scale=1e3),
-        xt.TargetList(['x', 'y'], at='e.ds.l2.b2', line='lhcb2', value='preserve', tol=1e-5, scale=1),
-        xt.TargetList(['px', 'py'], at='e.ds.l2.b2', line='lhcb2', value='preserve', tol=1e-5, scale=1e3),
-    ],
-    vary=
-        [xt.Vary('on_sep2', step=1e-4),
-         xt.VaryList([
-            # correctors to control the crossing angles
-            'corr_co_acbyvs4.l2b1', 'corr_co_acbyhs4.l2b1',
-            'corr_co_acbyvs4.r2b2', 'corr_co_acbyhs4.r2b2',
-             # correctors to close the bumps
-            'corr_co_acbyvs4.l2b2', 'corr_co_acbyhs4.l2b2',
-            'corr_co_acbyvs4.r2b1', 'corr_co_acbyhs4.r2b1',
-            'corr_co_acbyhs5.l2b2', 'corr_co_acbyvs5.l2b2',
-            'corr_co_acbchs5.r2b1', 'corr_co_acbcvs5.r2b1'],
-            step=1e-7),
-        ],
-)
+for ip_name in config_lumi_leveling_ip2_ip8.keys():
 
-# Leveling in IP8
-print('\n --- Leveling in IP8 ---')
-ip_name = 'ip8'
-config_this_ip = config_lumi_leveling_ip2_ip8[ip_name]
-bump_range = config_this_ip['bump_range']
+    print(f'\n --- Leveling in {ip_name} ---')
 
-beta0_b1 = collider.lhcb1.particle_ref.beta0[0]
-f_rev=1/(collider.lhcb1.get_length() /(beta0_b1 * clight))
+    config_this_ip = config_lumi_leveling_ip2_ip8[ip_name]
+    bump_range = config_this_ip['bump_range']
 
-targets=[]
-vary=[]
+    beta0_b1 = collider.lhcb1.particle_ref.beta0[0]
+    f_rev=1/(collider.lhcb1.get_length() /(beta0_b1 * clight))
 
+    targets=[]
+    vary=[]
 
-targets.append(
-    xt.TargetLuminosity(
-        ip_name=ip_name, luminosity=config_this_ip['luminosity'], crab=False,
-        tol=0.01 * config_this_ip['luminosity'],
-        f_rev=f_rev, num_colliding_bunches=config_this_ip['num_colliding_bunches'],
-        num_particles_per_bunch=config_beambeam['num_particles_per_bunch'],
-        sigma_z=config_beambeam['sigma_z'],
-        nemitt_x=config_beambeam['nemitt_x'],
-        nemitt_y=config_beambeam['nemitt_y'])
-)
+    if 'luminosity' in config_this_ip.keys():
+        targets.append(
+            xt.TargetLuminosity(
+                ip_name=ip_name, luminosity=config_this_ip['luminosity'], crab=False,
+                tol=0.01 * config_this_ip['luminosity'],
+                f_rev=f_rev, num_colliding_bunches=config_this_ip['num_colliding_bunches'],
+                num_particles_per_bunch=config_beambeam['num_particles_per_bunch'],
+                sigma_z=config_beambeam['sigma_z'],
+                nemitt_x=config_beambeam['nemitt_x'],
+                nemitt_y=config_beambeam['nemitt_y'])
+        )
+    elif 'separation_in_sigmas' in config_this_ip.keys():
+        targets.append(
+            xt.TargetSeparation(
+                ip_name=ip_name,
+                separation_norm=config_this_ip['separation_in_sigmas'],
+                tol=1e-4, # in sigmas
+                plane=config_this_ip['plane'],
+                nemitt_x=config_beambeam['nemitt_x'],
+                nemitt_y=config_beambeam['nemitt_y'])
+        )
+    else:
+        raise ValueError('Either `luminosity` or `separation_in_sigmas` must be specified')
 
-if config_this_ip['impose_separation_orthogonal_to_crossing']:
-    targets.append(
-        xt.TargetSeparationOrthogonalToCrossing(ip_name='ip8'))
-vary.append(
-    xt.VaryList(config_this_ip['knobs'], step=1e-4)) # to control separation in x and y
+    if config_this_ip['impose_separation_orthogonal_to_crossing']:
+        targets.append(
+            xt.TargetSeparationOrthogonalToCrossing(ip_name='ip8'))
+    vary.append(
+        xt.VaryList(config_this_ip['knobs'], step=1e-4)) # to control separation in x and y
 
-# Target and knobs to rematch the crossing angles and close the bumps
-for line_name in ['lhcb1', 'lhcb2']:
-    targets += [
-        # Preserve crossing angle
-        xt.TargetList(['px', 'py'], at=ip_name, line=line_name, value='preserve', tol=1e-7, scale=1e3),
-        # Close the bumps
-        xt.TargetList(['x', 'y'], at=bump_range[line_name][-1], line=line_name, value='preserve', tol=1e-5, scale=1),
-        xt.TargetList(['px', 'py'], at=bump_range[line_name][-1], line=line_name, value='preserve', tol=1e-5, scale=1e3),
-    ]
+    # Target and knobs to rematch the crossing angles and close the bumps
+    for line_name in ['lhcb1', 'lhcb2']:
+        targets += [
+            # Preserve crossing angle
+            xt.TargetList(['px', 'py'], at=ip_name, line=line_name, value='preserve', tol=1e-7, scale=1e3),
+            # Close the bumps
+            xt.TargetList(['x', 'y'], at=bump_range[line_name][-1], line=line_name, value='preserve', tol=1e-5, scale=1),
+            xt.TargetList(['px', 'py'], at=bump_range[line_name][-1], line=line_name, value='preserve', tol=1e-5, scale=1e3),
+        ]
 
-vary.append(xt.VaryList(config_this_ip['corrector_knob_names'], step=1e-7))
+    vary.append(xt.VaryList(config_this_ip['corrector_knob_names'], step=1e-7))
 
-# Leveling with crossing angle and bump rematching
-collider.match(
-    lines=['lhcb1', 'lhcb2'],
-    ele_start=[bump_range['lhcb1'][0], bump_range['lhcb2'][0]],
-    ele_stop=[bump_range['lhcb1'][-1], bump_range['lhcb2'][-1]],
-    twiss_init='preserve',
-    targets=targets,
-    vary=vary
-)
+    # Leveling with crossing angle and bump rematching
+    collider.match(
+        lines=['lhcb1', 'lhcb2'],
+        ele_start=[bump_range['lhcb1'][0], bump_range['lhcb2'][0]],
+        ele_stop=[bump_range['lhcb1'][-1], bump_range['lhcb2'][-1]],
+        twiss_init='preserve',
+        targets=targets,
+        vary=vary
+    )
 
 # Re-match tunes, and chromaticities
 conf_knobs_and_tuning = config['config_knobs_and_tuning']
