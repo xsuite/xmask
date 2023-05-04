8a5c209c5b5c503db72c7cda7ffc1a87448a6b89
giadarol <giovanni.iadarola@cern.ch>
2023-05-04 18:15:52 +0200

    Add footprint script

---
+++ examples/run3_ions/005_footprint.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import xtrack as xt
+
+collider = xt.Multiline.from_json('./collider_04_tuned_and_leveled_bb_on.json')
+collider.build_trackers()
+
+fp = collider['lhcb1'].get_footprint(
+    nemitt_x=1.65e-6, nemitt_y=1.65e-6)
+
+fp.plot()
+
+plt.show()
