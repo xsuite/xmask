623a644fe0ebb27dcbc823287432cc167478b356
giadarol <giovanni.iadarola@cern.ch>
2023-05-06 11:28:52 +0200

    A bunch of asserts

--- examples/explore_leveling/002_multiline_match.py
+++ examples/explore_leveling/002_multiline_match.py
@@ -6,6 +6,13 @@ collider = xt.Multiline.from_json('../hllhc15_collision/collider_02_tuned_bb_off
 collider.build_trackers()
 
 tw = collider.twiss(lines=['lhcb1', 'lhcb2'])
+assert tuple(tw._line_names) == ('lhcb1', 'lhcb2')
+assert 'mqs.23r2.b1' in tw.lhcb1.name
+assert 'mqs.23l4.b2' in tw.lhcb2.name
+assert tw.lhcb1['s', 'ip5'] < tw.lhcb1['s', 'ip6']
+assert tw.lhcb2['s', 'ip5'] > tw.lhcb2['s', 'ip6']
+
+
 
 collider.match(
     lines=['lhcb1', 'lhcb2'],
@@ -16,9 +23,11 @@ collider.match(
         xt.Vary('kqtd.b2', step=1e-8),
     ],
     targets = [
-        xt.Target('qx', line='lhcb1', value=62.31, tol=1e-4),
-        xt.Target('qy', line='lhcb1', value=60.32, tol=1e-4),
+        xt.Target('qx', line='lhcb1', value=62.317, tol=1e-4),
+        xt.Target('qy', line='lhcb1', value=60.327, tol=1e-4),
         xt.Target('qx', line='lhcb2', value=62.315, tol=1e-4),
         xt.Target('qy', line='lhcb2', value=60.325, tol=1e-4)
         ]
     )
+
+
