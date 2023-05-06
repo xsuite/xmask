8c61e3bc28c6fb4fff7aa2e641cca95a224e1877
giadarol <giovanni.iadarola@cern.ch>
2023-05-06 14:48:57 +0200

    Add test for multiline match

--- examples/explore_leveling/002_multiline_match.py
+++ examples/explore_leveling/002_multiline_match.py
@@ -17,6 +17,15 @@ assert np.isclose(tw.lhcb1.qy, 60.32, atol=1e-4, rtol=0)
 assert np.isclose(tw.lhcb2.qx, 62.31, atol=1e-4, rtol=0)
 assert np.isclose(tw.lhcb2.qy, 60.32, atol=1e-4, rtol=0)
 
+tw_part = collider.twiss(
+    lines=['lhcb1', 'lhcb2'],
+    ele_start=['ip5', 'ip6'],
+    ele_stop=['ip6', 'ip5'],
+    twiss_init=[tw.lhcb1.get_twiss_init(at_element='ip5'), tw.lhcb2.get_twiss_init(at_element='ip6')]
+)
+
+# Add some asserts here
+
 collider.match(
     lines=['lhcb1', 'lhcb2'],
     vary=[
@@ -45,31 +54,29 @@ assert np.isclose(tw1.lhcb2.qx, 62.315, atol=1e-4, rtol=0)
 assert np.isclose(tw1.lhcb2.qy, 60.325, atol=1e-4, rtol=0)
 
 # Match bumps in the two likes
-import pdb; pdb.set_trace()
 collider.match(
-    verbose=True,
     lines=['lhcb1', 'lhcb2'],
-    ele_start=['mq.33l8.b1', 'mq.23l8.b2'],
-    ele_stop=['mq.23l8.b1', 'mq.33l8.b2'],
+    ele_start=['mq.33l8.b1', 'mq.22l8.b2'],
+    ele_stop=['mq.23l8.b1', 'mq.32l8.b2'],
     twiss_init='preserve',
     vary=[
         xt.VaryList([
             'acbv30.l8b1', 'acbv28.l8b1', 'acbv26.l8b1', 'acbv24.l8b1'],
             step=1e-10),
         xt.VaryList([
-            'acbh30.l8b2', 'acbh28.l8b2', 'acbh26.l8b2', 'acbh24.l8b2'],
+            'acbv29.l8b2', 'acbv27.l8b2', 'acbv25.l8b2', 'acbv23.l8b2'],
             step=1e-10),
     ],
     targets=[
         xt.Target('y', at='mb.b28l8.b1', line='lhcb1', value=3e-3, tol=1e-4, scale=1),
         xt.Target('py', at='mb.b28l8.b1', line='lhcb1', value=0, tol=1e-6, scale=1000),
-        xt.Target('x', at='mb.b28l8.b2', line='lhcb2', value=2e-3, tol=1e-4, scale=1),
-        xt.Target('px', at='mb.b28l8.b2', line='lhcb2', value=0, tol=1e-6, scale=1000),
+        xt.Target('y', at='mb.b27l8.b2', line='lhcb2', value=2e-3, tol=1e-4, scale=1),
+        xt.Target('py', at='mb.b27l8.b2', line='lhcb2', value=0, tol=1e-6, scale=1000),
         # I want the bump to be closed
         xt.TargetList(['y'], at='mq.23l8.b1', line='lhcb1', value='preserve', tol=1e-6, scale=1),
         xt.TargetList(['py'], at='mq.23l8.b1', line='lhcb1', value='preserve', tol=1e-7, scale=1000),
-        xt.TargetList(['x'], at='mq.33l8.b2', line='lhcb2', value='preserve', tol=1e-6, scale=1),
-        xt.Target('px', at='mq.33l8.b2', line='lhcb2', value='preserve', tol=1e-10, scale=1000),
+        xt.TargetList(['y'], at='mq.32l8.b2', line='lhcb2', value='preserve', tol=1e-6, scale=1),
+        xt.Target('py', at='mq.32l8.b2', line='lhcb2', value='preserve', tol=1e-10, scale=1000),
     ]
 )
 tw_bump = collider.twiss(lines=['lhcb1', 'lhcb2'])
@@ -83,19 +90,10 @@ assert np.isclose(tw_bump.lhcb1['y', 'mq.33l8.b1'], tw_before['y', 'mq.33l8.b1']
 assert np.isclose(tw_bump.lhcb1['py', 'mq.33l8.b1'], tw_before['py', 'mq.33l8.b1'], atol=1e-7)
 
 tw_before = tw1.lhcb2
-assert np.isclose(tw_bump.lhcb2['x', 'mb.b28l8.b2'], 2e-3, atol=1e-4)
-assert np.isclose(tw_bump.lhcb2['px', 'mb.b28l8.b2'], 0, atol=1e-6)
-assert np.isclose(tw_bump.lhcb2['x', 'mq.33l8.b2'], tw_before['x', 'mq.33l8.b2'], atol=1e-6)
-assert np.isclose(tw_bump.lhcb2['px', 'mq.33l8.b2'], tw_before['px', 'mq.33l8.b2'], atol=1e-7)
-assert np.isclose(tw_bump.lhcb2['x', 'mq.23l8.b2'], tw_before['x', 'mq.23l8.b2'], atol=1e-6)
-assert np.isclose(tw_bump.lhcb2['px', 'mq.23l8.b2'], tw_before['px', 'mq.23l8.b2'], atol=1e-7)
+assert np.isclose(tw_bump.lhcb2['y', 'mb.b27l8.b2'], 2e-3, atol=1e-4)
+assert np.isclose(tw_bump.lhcb2['py', 'mb.b27l8.b2'], 0, atol=1e-6)
+assert np.isclose(tw_bump.lhcb2['y', 'mq.32l8.b2'], tw_before['y', 'mq.33l8.b2'], atol=1e-6)
+assert np.isclose(tw_bump.lhcb2['py', 'mq.32l8.b2'], tw_before['py', 'mq.33l8.b2'], atol=1e-7)
+assert np.isclose(tw_bump.lhcb2['y', 'mq.22l8.b2'], tw_before['y', 'mq.23l8.b2'], atol=1e-6)
+assert np.isclose(tw_bump.lhcb2['py', 'mq.22l8.b2'], tw_before['py', 'mq.23l8.b2'], atol=1e-7)
 
-# Seem to have found a bug in the twiss
-tw_bump_part = collider.twiss(lines=['lhcb1', 'lhcb2'],
-    ele_start=['mq.33l8.b1', 'mq.23l8.b2'],
-    ele_stop=['mq.23l8.b1', 'mq.33l8.b2'],
-    twiss_init='preserve')
-
-# These two are not the same!!!!
-tw_bump_part.lhcb2['px', 'mq.23l8.b2']
-tw_bump.lhcb2['px', 'mq.23l8.b2']
--- tests/test_hllhc14.py
+++ tests/test_hllhc14.py
@@ -1520,3 +1520,97 @@ def test_apply_filling_scheme():
     all_bb_lenses_b2 = twb2.rows['bb_.*'].name
     assert np.all(np.array([collider.lhcb2[nn].scale_strength for nn in all_bb_lenses_b2]) == 1)
 
+def test_multiline_match():
+    collider = xt.Multiline.from_json('collider_hllhc14_02.json')
+    collider.build_trackers()
+
+    tw = collider.twiss(lines=['lhcb1', 'lhcb2'])
+    assert tuple(tw._line_names) == ('lhcb1', 'lhcb2')
+    assert 'mqs.23r2.b1' in tw.lhcb1.name
+    assert 'mqs.23l4.b2' in tw.lhcb2.name
+    assert tw.lhcb1['s', 'ip5'] < tw.lhcb1['s', 'ip6']
+    assert tw.lhcb2['s', 'ip5'] > tw.lhcb2['s', 'ip6']
+    assert np.isclose(tw.lhcb1.qx, 62.31, atol=1e-4, rtol=0)
+    assert np.isclose(tw.lhcb1.qy, 60.32, atol=1e-4, rtol=0)
+    assert np.isclose(tw.lhcb2.qx, 62.315, atol=1e-4, rtol=0)
+    assert np.isclose(tw.lhcb2.qy, 60.325, atol=1e-4, rtol=0)
+
+    tw_part = collider.twiss(
+        lines=['lhcb1', 'lhcb2'],
+        ele_start=['ip5', 'ip6'],
+        ele_stop=['ip6', 'ip5'],
+        twiss_init=[tw.lhcb1.get_twiss_init(at_element='ip5'), tw.lhcb2.get_twiss_init(at_element='ip6')]
+    )
+
+    # Add some asserts here
+
+    collider.match(
+        lines=['lhcb1', 'lhcb2'],
+        vary=[
+            xt.Vary('kqtf.b1', step=1e-8),
+            xt.Vary('kqtd.b1', step=1e-8),
+            xt.Vary('kqtf.b2', step=1e-8),
+            xt.Vary('kqtd.b2', step=1e-8),
+        ],
+        targets = [
+            xt.Target('qx', line='lhcb1', value=62.317, tol=1e-4),
+            xt.Target('qy', line='lhcb1', value=60.327, tol=1e-4),
+            xt.Target('qx', line='lhcb2', value=62.313, tol=1e-4),
+            xt.Target('qy', line='lhcb2', value=60.323, tol=1e-4)
+            ]
+        )
+
+    tw1 = collider.twiss(lines=['lhcb1', 'lhcb2'])
+    assert tuple(tw1._line_names) == ('lhcb1', 'lhcb2')
+    assert 'mqs.23r2.b1' in tw1.lhcb1.name
+    assert 'mqs.23l4.b2' in tw1.lhcb2.name
+    assert tw1.lhcb1['s', 'ip5'] < tw1.lhcb1['s', 'ip6']
+    assert tw1.lhcb2['s', 'ip5'] > tw1.lhcb2['s', 'ip6']
+    assert np.isclose(tw1.lhcb1.qx, 62.317, atol=1e-4, rtol=0)
+    assert np.isclose(tw1.lhcb1.qy, 60.327, atol=1e-4, rtol=0)
+    assert np.isclose(tw1.lhcb2.qx, 62.313, atol=1e-4, rtol=0)
+    assert np.isclose(tw1.lhcb2.qy, 60.323, atol=1e-4, rtol=0)
+
+    # Match bumps in the two likes
+    collider.match(
+        lines=['lhcb1', 'lhcb2'],
+        ele_start=['mq.33l8.b1', 'mq.22l8.b2'],
+        ele_stop=['mq.23l8.b1', 'mq.32l8.b2'],
+        twiss_init='preserve',
+        vary=[
+            xt.VaryList([
+                'acbv30.l8b1', 'acbv28.l8b1', 'acbv26.l8b1', 'acbv24.l8b1'],
+                step=1e-10),
+            xt.VaryList([
+                'acbv29.l8b2', 'acbv27.l8b2', 'acbv25.l8b2', 'acbv23.l8b2'],
+                step=1e-10),
+        ],
+        targets=[
+            xt.Target('y', at='mb.b28l8.b1', line='lhcb1', value=3e-3, tol=1e-4, scale=1),
+            xt.Target('py', at='mb.b28l8.b1', line='lhcb1', value=0, tol=1e-6, scale=1000),
+            xt.Target('y', at='mb.b27l8.b2', line='lhcb2', value=2e-3, tol=1e-4, scale=1),
+            xt.Target('py', at='mb.b27l8.b2', line='lhcb2', value=0, tol=1e-6, scale=1000),
+            # I want the bump to be closed
+            xt.TargetList(['y'], at='mq.23l8.b1', line='lhcb1', value='preserve', tol=1e-6, scale=1),
+            xt.TargetList(['py'], at='mq.23l8.b1', line='lhcb1', value='preserve', tol=1e-7, scale=1000),
+            xt.TargetList(['y'], at='mq.32l8.b2', line='lhcb2', value='preserve', tol=1e-6, scale=1),
+            xt.Target('py', at='mq.32l8.b2', line='lhcb2', value='preserve', tol=1e-10, scale=1000),
+        ]
+    )
+    tw_bump = collider.twiss(lines=['lhcb1', 'lhcb2'])
+
+    tw_before = tw1.lhcb1
+    assert np.isclose(tw_bump.lhcb1['y', 'mb.b28l8.b1'], 3e-3, atol=1e-4)
+    assert np.isclose(tw_bump.lhcb1['py', 'mb.b28l8.b1'], 0, atol=1e-6)
+    assert np.isclose(tw_bump.lhcb1['y', 'mq.23l8.b1'], tw_before['y', 'mq.23l8.b1'], atol=1e-6)
+    assert np.isclose(tw_bump.lhcb1['py', 'mq.23l8.b1'], tw_before['py', 'mq.23l8.b1'], atol=1e-7)
+    assert np.isclose(tw_bump.lhcb1['y', 'mq.33l8.b1'], tw_before['y', 'mq.33l8.b1'], atol=1e-6)
+    assert np.isclose(tw_bump.lhcb1['py', 'mq.33l8.b1'], tw_before['py', 'mq.33l8.b1'], atol=1e-7)
+
+    tw_before = tw1.lhcb2
+    assert np.isclose(tw_bump.lhcb2['y', 'mb.b27l8.b2'], 2e-3, atol=1e-4)
+    assert np.isclose(tw_bump.lhcb2['py', 'mb.b27l8.b2'], 0, atol=1e-6)
+    assert np.isclose(tw_bump.lhcb2['y', 'mq.32l8.b2'], tw_before['y', 'mq.33l8.b2'], atol=1e-6)
+    assert np.isclose(tw_bump.lhcb2['py', 'mq.32l8.b2'], tw_before['py', 'mq.33l8.b2'], atol=1e-7)
+    assert np.isclose(tw_bump.lhcb2['y', 'mq.22l8.b2'], tw_before['y', 'mq.23l8.b2'], atol=1e-6)
+    assert np.isclose(tw_bump.lhcb2['py', 'mq.22l8.b2'], tw_before['py', 'mq.23l8.b2'], atol=1e-7)
