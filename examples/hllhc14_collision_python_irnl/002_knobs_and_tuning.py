ace39363fa81fe9fa6e69acf5e0c45b54258c879
JoschD <26184899+JoschD@users.noreply.github.com>
2023-10-02 16:56:11 +0200

    run for gianni

--- examples/hllhc14_collision_python_irnl/002_knobs_and_tuning.py
+++ examples/hllhc14_collision_python_irnl/002_knobs_and_tuning.py
@@ -20,19 +20,19 @@ for line_name in ['lhcb1', 'lhcb2']:
     if conf_knobs_and_tuning['ir_rdt_correction_enabled']:
         conf_knobs_and_tuning['ir_rdt_correction']["line_name"] = line_name
         
-        xm.machine_tuning(line=collider[line_name],
-            enable_closed_orbit_correction=True,
-            enable_linear_coupling_correction=True,
-            enable_tune_correction=True,
-            enable_chromaticity_correction=True,
-            dual_pass_tune_and_chroma=True,
-            coupling_correction_analytical_estimation=True,
-            coupling_correction_iterative_estimation=5,
-            knob_names=conf_knobs_and_tuning['knob_names'][line_name],
-            targets=conf_knobs_and_tuning["targets"][line_name],
-            line_co_ref=collider[f'{line_name}_co_ref'],
-            co_corr_config=conf_knobs_and_tuning['closed_orbit_correction'][line_name],
-        )
+        # xm.machine_tuning(line=collider[line_name],
+        #     enable_closed_orbit_correction=True,
+        #     enable_linear_coupling_correction=True,
+        #     enable_tune_correction=True,
+        #     enable_chromaticity_correction=True,
+        #     dual_pass_tune_and_chroma=True,
+        #     coupling_correction_analytical_estimation=True,
+        #     coupling_correction_iterative_estimation=5,
+        #     knob_names=conf_knobs_and_tuning['knob_names'][line_name],
+        #     targets=conf_knobs_and_tuning["targets"][line_name],
+        #     line_co_ref=collider[f'{line_name}_co_ref'],
+        #     co_corr_config=conf_knobs_and_tuning['closed_orbit_correction'][line_name],
+        # )
 
         xm.correct_errors(line=collider[line_name],
             enable_ir_rdt_correction=conf_knobs_and_tuning['ir_rdt_correction_enabled'],
