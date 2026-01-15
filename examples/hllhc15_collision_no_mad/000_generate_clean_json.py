af3e85461e568ebc2dfe7ae4da04e1b610400d92
giadarol <giovanni.iadarola@cern.ch>
2026-01-15 18:30:11 +0100

    Mismatch

--- examples/hllhc15_collision_no_mad/000_generate_clean_json.py
+++ examples/hllhc15_collision_no_mad/000_generate_clean_json.py
@@ -33,7 +33,43 @@ lhc.lines['b2'] = lhc.lhcb2
 del lhc.lines['lhcb1']
 del lhc.lines['lhcb2']
 
-lhc.vars.load('opt_round_150_1500.madx')
+for ll in ['b1', 'b2']:
+    lhc.lines[ll].slice_thick_elements(
+        slicing_strategies=[
+            xt.Strategy(slicing=xt.Teapot(1)),
+            xt.Strategy(element_type=xt.UniformSolenoid, slicing=None),
+            xt.Strategy(name=r'mb\..*',    slicing=xt.Teapot(2)),
+            xt.Strategy(name=r'mq\..*',    slicing=xt.Teapot(2)),
+            xt.Strategy(name=r'mqxa\..*',  slicing=xt.Teapot(16)),  # old triplet
+            xt.Strategy(name=r'mqxb\..*',  slicing=xt.Teapot(16)),  # old triplet
+            xt.Strategy(name=r'mqxc\..*',  slicing=xt.Teapot(16)),  # new mqxa (q1,q3)
+            xt.Strategy(name=r'mqxd\..*',  slicing=xt.Teapot(16)),  # new mqxb (q2a,q2b)
+            xt.Strategy(name=r'mqxfa\..*', slicing=xt.Teapot(16)),  # new (q1,q3 v1.1)
+            xt.Strategy(name=r'mqxfb\..*', slicing=xt.Teapot(16)),  # new (q2a,q2b v1.1)
+            xt.Strategy(name=r'mbxa\..*',  slicing=xt.Teapot(4)),   # new d1
+            xt.Strategy(name=r'mbxf\..*',  slicing=xt.Teapot(4)),   # new d1 (v1.1)
+            xt.Strategy(name=r'mbrd\..*',  slicing=xt.Teapot(4)),   # new d2 (if needed)
+            xt.Strategy(name=r'mqyy\..*',  slicing=xt.Teapot(4)),   # new q4
+            xt.Strategy(name=r'mqyl\..*',  slicing=xt.Teapot(4)),   # new q5
+            xt.Strategy(name=r'mbx\..*',    slicing=xt.Teapot(4)),
+            xt.Strategy(name=r'mbrb\..*',   slicing=xt.Teapot(4)),
+            xt.Strategy(name=r'mbrc\..*',   slicing=xt.Teapot(4)),
+            xt.Strategy(name=r'mbrs\..*',   slicing=xt.Teapot(4)),
+            xt.Strategy(name=r'mbh\..*',    slicing=xt.Teapot(4)),
+            xt.Strategy(name=r'mqwa\..*',   slicing=xt.Teapot(4)),
+            xt.Strategy(name=r'mqwb\..*',   slicing=xt.Teapot(4)),
+            xt.Strategy(name=r'mqy\..*',    slicing=xt.Teapot(4)),
+            xt.Strategy(name=r'mqm\..*',    slicing=xt.Teapot(4)),
+            xt.Strategy(name=r'mqmc\..*',   slicing=xt.Teapot(4)),
+            xt.Strategy(name=r'mqml\..*',   slicing=xt.Teapot(4)),
+            xt.Strategy(name=r'mqtlh\..*',  slicing=xt.Teapot(2)),
+            xt.Strategy(name=r'mqtli\..*',  slicing=xt.Teapot(2)),
+            xt.Strategy(name=r'mqt\..*',    slicing=xt.Teapot(2)),
+])
+
+lhc.vars.load('opt_round_150_1500_thin.madx')
+
+prrrrr
 
 ########################
 # Match coupling knobs #
@@ -111,8 +147,6 @@ lhc.b1['c_minus_re.b1'] = 0
 lhc.b1['c_minus_im.b1'] = 0
 lhc.b2['c_minus_re.b2'] = 0
 lhc.b2['c_minus_im.b2'] = 0
-
-
 
 lhc.b1.particle_ref = None
 lhc.b2.particle_ref = None
