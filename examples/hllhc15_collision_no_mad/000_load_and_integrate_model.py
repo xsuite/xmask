2dfa0dd67bc693ce306370739fbb4679ae6e824c
giadarol <giovanni.iadarola@cern.ch>
2026-01-16 10:17:31 +0100

    Working on octupole circuits

--- examples/hllhc15_collision_no_mad/000_load_and_integrate_model.py
+++ examples/hllhc15_collision_no_mad/000_load_and_integrate_model.py
@@ -15,13 +15,36 @@ lhc.b2.twiss_default.clear()
 # Load optics
 lhc.vars.load(config['optics_file'])
 
-# Attach reference particle (TODO: generalize for ions)
-for bb in ['b1','b2']:
-    lhc[bb].set_particle_ref('proton',
-         energy0=config['beam_config'][bb]['beam_energy_tot'] * 1e9)
+# Create reference particles (TODO: generalize for ions)
+lhc.new_particle(f'particle_ref_b1',
+            energy0=config['beam_config']['b1']['beam_energy_tot'] * 1e9)
+lhc.new_particle(f'particle_ref_b2',
+            energy0=config['beam_config']['b2']['beam_energy_tot'] * 1e9)
+
+# Assign reference particles to beams
+lhc.b1.particle_ref = 'particle_ref_b1'
+lhc.b2.particle_ref = 'particle_ref_b2'
+
+# Define reference rigidity variables
+lhc['brho0_b1'] = lhc.ref['particle_ref_b1'].rigidity0[0]
+lhc['brho0_b2'] = lhc.ref['particle_ref_b2'].rigidity0[0]
+
+# Define new knobs
+for knob_name, knob_expr in config['new_knobs'].items():
+    lhc[knob_name] = knob_expr
+
+# Define experimental magnet knobs
+
+# Attach orbit correction knobs to all dipole correctors
+
+# Prepare reference model for orbit correction
+
 
 twb1 = lhc.b1.twiss4d()
 twb2 = lhc.b2.twiss4d()
+
+
+
 
 
 
