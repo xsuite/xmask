a3e74aac51e700648963e82aec9282ad4d940bee
giadarol <giovanni.iadarola@cern.ch>
2026-01-21 22:38:40 +0100

    class

--- examples/dev_error_corrections/000_dev.py
+++ examples/dev_error_corrections/000_dev.py
@@ -5,41 +5,48 @@ env = xt.load('collider_00_from_mad.json')
 
 tw = env.lhcb1_co_ref.twiss4d() # Reference twiss
 
-# Let's try to compute the IP5 b4 correctors (kcsx3.l5 and kcsx3.r5)
-line = env.lhcb1
-start = 'dfxj.4l5'
-end = 'dfxj.4r5'
-correction_knobs = ['kcox3.l5', 'kcox3.r5']
-multipole = 'k3l'
-rdt_indeces = [(0, 4), (4, 0)]
+class RDTCorrector:
+    def __init__(self, env, tw):
+        self.env = env
+        self.tw = tw
+        self.line = env.lhcb1
+        self.start = 'dfxj.4l5'
+        self.end = 'dfxj.4r5'
+        self.correction_knobs = ['kcox3.l5', 'kcox3.r5']
+        self.multipole = 'k3l'
+        self.rdt_indices = [(0, 4), (4, 0)]
+        self.rdt_terms = {}
 
-# for kk in correction_knobs:
-#     env[kk] = 0.0
+    def run(self):
+        tt = self.line.get_table(attr=True)
+        tt_range = tt.rows[self.start:self.end]
 
-tt = line.get_table(attr=True)
+        # Identify elements controlled by correction knobs
+        elements_in_range = set(tt_range.env_name)
+        correction_elements = []
+        for kk in self.correction_knobs:
+            for tt in self.line.ref[kk]._find_dependant_targets():
+                if isinstance(tt, xd.refs.ItemRef) and tt._key in elements_in_range:
+                    correction_elements.append(tt._key)
 
-tt_range = tt.rows[start:end]
+        mask_corr = tt_range.rows.mask[list(correction_elements)]
+        tt_integral = tt_range.rows[(tt_range[self.multipole] != 0) | (mask_corr)]
 
-# Identify elements controlled by correction knobs
-elements_in_range = set(tt_range.env_name)
-correction_elements = []
-for kk in correction_knobs:
-    for tt in line.ref[kk]._find_dependant_targets():
-        if isinstance(tt, xd.refs.ItemRef) and tt._key in elements_in_range:
-            correction_elements.append(tt._key)
+        ref_names = [nn.replace('/lhcb1', '') for nn in tt_integral.env_name]
+        tw_integral = self.tw.rows[ref_names]
 
-mask_corr = tt_range.rows.mask[list(correction_elements)]
-tt_integral = tt_range.rows[(tt_range[multipole] != 0) | (mask_corr)]
+        for rdt_i in self.rdt_indices:
+            assert len(rdt_i) == 2
+            ii = rdt_i[0]
+            jj = rdt_i[1]
 
-ref_names = [nn.replace('/lhcb1', '') for nn in tt_integral.env_name]
-tw_integral = tw.rows[ref_names]
-rdt_terms = {}
-for rdt_i in rdt_indeces:
-    assert len(rdt_i) == 2
-    ii = rdt_i[0]
-    jj = rdt_i[1]
+            r_ii_jj = (tw_integral.betx ** (ii / 2) * tw_integral.bety ** (jj / 2)
+                       * tt_integral[self.multipole])
+            self.rdt_terms[rdt_i] = r_ii_jj.sum()
 
-    r_ii_jj = (tw_integral.betx ** (ii / 2) * tw_integral.bety ** (jj / 2)
-               * tt_integral[multipole])
-    rdt_terms[rdt_i] = r_ii_jj.sum()
+        return self.rdt_terms
 
+
+# Usage:
+corrector = RDTCorrector(env, tw)
+rdt_terms = corrector.run()
