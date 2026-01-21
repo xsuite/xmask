b34a15aa08724b341e6e9a0065e206085081ced5
giadarol <giovanni.iadarola@cern.ch>
2026-01-21 20:50:31 +0100

    I can see that switching off the knob reduces the rdt

--- examples/dev_error_corrections/000_dev.py
+++ examples/dev_error_corrections/000_dev.py
@@ -1,4 +1,5 @@
 import xtrack as xt
+import xdeps as xd
 
 env = xt.load('collider_00_from_mad.json')
 
@@ -8,7 +9,37 @@ tw = env.lhcb1_co_ref.twiss4d() # Reference twiss
 line = env.lhcb1
 start = 'dfxj.4l5'
 end = 'dfxj.4r5'
+correction_knobs = ['kcox3.l5', 'kcox3.r5']
+multipole = 'k3l'
+rdt_indeces = [(0, 4), (4, 0)]
+
+# for kk in correction_knobs:
+#     env[kk] = 0.0
 
 tt = line.get_table(attr=True)
 
 tt_range = tt.rows[start:end]
+
+# Identify elements controlled by correction knobs
+elements_in_range = set(tt_range.env_name)
+correction_elements = []
+for kk in correction_knobs:
+    for tt in line.ref[kk]._find_dependant_targets():
+        if isinstance(tt, xd.refs.ItemRef) and tt._key in elements_in_range:
+            correction_elements.append(tt._key)
+
+mask_corr = tt_range.rows.mask[list(correction_elements)]
+tt_integral = tt_range.rows[(tt_range[multipole] != 0) | (mask_corr)]
+
+ref_names = [nn.replace('/lhcb1', '') for nn in tt_integral.env_name]
+tw_integral = tw.rows[ref_names]
+rdt_terms = {}
+for rdt_i in rdt_indeces:
+    assert len(rdt_i) == 2
+    ii = rdt_i[0]
+    jj = rdt_i[1]
+
+    r_ii_jj = (tw_integral.betx ** (ii / 2) * tw_integral.bety ** (jj / 2)
+               * tt_integral[multipole])
+    rdt_terms[rdt_i] = r_ii_jj.sum()
+
