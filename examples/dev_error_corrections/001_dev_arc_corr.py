2c1fdbd2bf8e217cf3b31a8e416724cbf051c104
giadarol <giovanni.iadarola@cern.ch>
2026-01-22 18:19:39 +0100

    Remove correction

--- examples/dev_error_corrections/001_dev_arc_corr.py
+++ examples/dev_error_corrections/001_dev_arc_corr.py
@@ -5,11 +5,11 @@ beam_name = 'b1'
 env = xt.load('collider_00_from_mad_with_errors.json')
 line = env[f'lhc{beam_name}']
 
-tw = env.lhcb1_co_ref.twiss4d() # Reference twiss
+# Reference twiss
+env_no_err = xt.load('collider_00_from_mad_no_errors.json')
+tw = env_no_err[f'lhc{beam_name}'].twiss4d() # Reference twiss
 
 # Let's have a look at the b5 (k4)
-
-tt = line.get_table(attr=True)
 
 arc_name = '45'
 start = f's.ds.r{arc_name[0]}.{beam_name}'
@@ -17,6 +17,10 @@ end = f'e.ds.r{arc_name[1]}.{beam_name}'
 correction_knobs = [f'kcd.a45{beam_name}']
 multipole = 'k4l'
 
+for kk in correction_knobs:
+    env[kk] = 0.0
+
+tt = line.get_table(attr=True)
 tt_range = tt.rows[start:end]
 
 # Identify elements controlled by correction knobs
@@ -29,7 +33,4 @@ for kk in correction_knobs:
 
 mask_corr = tt_range.rows.mask[list(correction_elements)]
 tt_integral = tt_range.rows[(tt_range[multipole] != 0) | (mask_corr)]
-
-
-ref_names = [nn.replace('/lhcb1', '/lhcb1_co_ref') for nn in tt_integral.env_name]
-tw_integral = tw.rows[ref_names]
+tw_integral = tw.rows[tt_integral.env_name]
