c7847ecd2c2f12f95a5ad6c1171c80d55c0c9396
giadarol <giovanni.iadarola@cern.ch>
2026-02-20 08:51:54 +0100

    Work in progress

--- examples/dev_error_corrections/001_dev_arc_corr.py
+++ examples/dev_error_corrections/001_dev_arc_corr.py
@@ -1,6 +1,9 @@
 import xtrack as xt
 import xdeps as xd
 
+# To do:
+# - Handle slice elements for correctors!!!
+
 beam_name = 'b1'
 env = xt.load('collider_00_from_mad_with_errors.json')
 line = env[f'lhc{beam_name}']
@@ -9,7 +12,7 @@ line = env[f'lhc{beam_name}']
 env_no_err = xt.load('collider_00_from_mad_no_errors.json')
 tw = env_no_err[f'lhc{beam_name}'].twiss4d() # Reference twiss
 
-# Let's have a look at the b5 (k4)
+# Let's have a look at the b5 (k4) # decapole
 
 arc_name = '45'
 start = f's.ds.r{arc_name[0]}.{beam_name}'
@@ -17,20 +20,24 @@ end = f'e.ds.r{arc_name[1]}.{beam_name}'
 correction_knobs = [f'kcd.a45{beam_name}']
 multipole = 'k4l'
 
-for kk in correction_knobs:
-    env[kk] = 0.0
+# for kk in correction_knobs:
+#     env[kk] = 0.0
 
-tt = line.get_table(attr=True)
-tt_range = tt.rows[start:end]
+tt0 = line.get_table(attr=True)
+tt0_range = tt0.rows[start:end]
 
 # Identify elements controlled by correction knobs
-elements_in_range = set(tt_range.env_name)
+elements_in_range = set(tt0_range.env_name)
 correction_elements = []
 for kk in correction_knobs:
-    for tt in line.ref[kk]._find_dependant_targets():
-        if isinstance(tt, xd.refs.ItemRef) and tt._key in elements_in_range:
-            correction_elements.append(tt._key)
+    for ttar in line.ref[kk]._find_dependant_targets():
+        if isinstance(ttar, xd.refs.ItemRef) and ttar._key in elements_in_range:
+            correction_elements.append(ttar._key)
 
-mask_corr = tt_range.rows.mask[list(correction_elements)]
-tt_integral = tt_range.rows[(tt_range[multipole] != 0) | (mask_corr)]
-tw_integral = tw.rows[tt_integral.env_name]
+mask_corr = tt0_range.rows.mask[list(correction_elements)]
+tt_integral = tt0_range.rows[(tt0_range[multipole] != 0) | (mask_corr)]
+tw_integral = tw.rows[tt_integral.name]
+
+
+
+# env[correction_knobs[0]] = 0
