3f42ea877b65b6cb7bc8ba2ec4396691b1212598
giadarol <giovanni.iadarola@cern.ch>
2026-02-21 08:06:54 +0100

    Clean up

--- examples/dev_error_corrections/001_dev_arc_corr.py
+++ examples/dev_error_corrections/001_dev_arc_corr.py
@@ -49,25 +49,3 @@ rdt_contrib.print_corrections()
 env[opt.knob_name] = 1.0
 print("After setting the knob:")
 rdt_contrib.print_corrections()
-
-# for kk in correction_knobs:
-#     env[kk] = 0.0
-
-# tt0 = line.get_table(attr=True)
-# tt0_range = tt0.rows[start:end]
-
-# # Identify elements controlled by correction knobs
-# elements_in_range = set(tt0_range.env_name)
-# correction_elements = []
-# for kk in correction_knobs:
-#     for ttar in line.ref[kk]._find_dependant_targets():
-#         if isinstance(ttar, xd.refs.ItemRef) and ttar._key in elements_in_range:
-#             correction_elements.append(ttar._key)
-
-# mask_corr = tt0_range.rows.mask[list(correction_elements)]
-# tt_integral = tt0_range.rows[(tt0_range[multipole] != 0) | (mask_corr)]
-# tw_integral = tw.rows[tt_integral.name]
-
-
-
-# env[correction_knobs[0]] = 0
