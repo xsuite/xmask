1bb994267ff22b55fc93cc078c77ea8536d063e7
giadarol <giovanni.iadarola@cern.ch>
2026-02-21 10:15:01 +0100

    Still something to understand

--- examples/dev_error_corrections/002_dev_arc_corr_a2.py
+++ examples/dev_error_corrections/002_dev_arc_corr_a2.py
@@ -26,6 +26,33 @@ def chorm_coupling_integrand(tw, tt):
 
 arcs = ['12', '23', '34', '45', '56', '67', '78', '81']
 
+# Global correction setup (we run it after logal)
+start = tw.name[0]
+end = tw.name[-1]
+correction_knobs = [f'kss.a{arc_name}{beam_name}' for arc_name in arcs]
+generated_knob_name = f'on_corr_k2sl_global'
+multipole = 'k2sl'
+target_quantities={
+    'chrom_coupling_real': lambda tw, tt: chorm_coupling_integrand(tw, tt).real,
+    'chrom_coupling_imag': lambda tw, tt: chorm_coupling_integrand(tw, tt).imag
+}
+
+rdt_contrib_glob = IntegralCorrection(
+                        line=env['lhcb1'],
+                        tw=tw,
+                        feed_down=True,
+                        start=start,
+                        end=end,
+                        correction_knobs=correction_knobs,
+                        multipole=multipole,
+                        ip=None,
+                        target_quantities=target_quantities,
+                        generated_knob_name=generated_knob_name,
+                        scale_multipole=scale_multipole)
+print("Original correction:")
+rdt_contrib_glob.print_corrections()
+
+opt_dct = {}
 for arc_name in arcs:
     start = f's.ds.r{arc_name[0]}.{beam_name}'
     end = f'e.ds.l{arc_name[1]}.{beam_name}'
@@ -63,39 +90,17 @@ for arc_name in arcs:
     print("After setting the knob:")
     rdt_contrib.print_corrections()
 
+    opt_dct[arc_name] = opt
+
+
 # Global correction
-start = tw.name[0]
-end = tw.name[-1]
-correction_knobs = [f'kss.a{arc_name}{beam_name}' for arc_name in arcs]
-generated_knob_name = f'on_corr_k2sl_global'
-multipole = 'k2sl'
-target_quantities={
-    'chrom_coupling_real': lambda tw, tt: chorm_coupling_integrand(tw, tt).real,
-    'chrom_coupling_imag': lambda tw, tt: chorm_coupling_integrand(tw, tt).imag
-}
 
-# Usage:
-rdt_contrib = IntegralCorrection(
-                        line=env['lhcb1'],
-                        tw=tw,
-                        feed_down=True,
-                        start=start,
-                        end=end,
-                        correction_knobs=correction_knobs,
-                        multipole=multipole,
-                        ip=None,
-                        target_quantities=target_quantities,
-                        generated_knob_name=generated_knob_name,
-                        scale_multipole=scale_multipole)
-print("Original correction:")
-rdt_contrib.print_corrections()
-
-rdt_contrib.clear_corrections()
-opt = rdt_contrib.correct()
+# rdt_contrib.clear_corrections() # BAD!!!
+opt = rdt_contrib_glob.correct()
 
 print("Before setting the knob:")
-rdt_contrib.print_corrections()
+rdt_contrib_glob.print_corrections()
 
 env[opt.knob_name] = 1.0
 print("After setting the knob:")
-rdt_contrib.print_corrections()
+rdt_contrib_glob.print_corrections()
