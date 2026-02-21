5d2efe76f4be2433f30a37bb1b615a6bfec47ff5
giadarol <giovanni.iadarola@cern.ch>
2026-02-21 09:52:36 +0100

    Local correction a3 in all arcs

--- examples/dev_error_corrections/002_dev_arc_corr_a2.py
+++ examples/dev_error_corrections/002_dev_arc_corr_a2.py
@@ -11,23 +11,6 @@ line = env[f'lhc{beam_name}']
 env_no_err = xt.load('collider_00_from_mad_no_errors.json')
 tw = env_no_err[f'lhc{beam_name}'].twiss4d() # Reference twiss
 
-# Let's have a look at the a3 (skew sextupole)
-
-def chorm_coupling_integrand(tw, tt):
-    return tt['k2sl'] * tw.dx * np.sqrt(tw.betx * tw.bety)  * np.exp(1j*(tw.mux - tw.muy))
-
-arc_name = '45'
-start = f's.ds.r{arc_name[0]}.{beam_name}'
-end = f'e.ds.l{arc_name[1]}.{beam_name}'
-correction_knobs = [f'kss.a45{beam_name}']
-multipole = 'k2sl'
-target_quantities={
-    'chrom_coupling_real': lambda tw, tt: chorm_coupling_integrand(tw, tt).real,
-    'chrom_coupling_imag': lambda tw, tt: chorm_coupling_integrand(tw, tt).imag
-}
-generated_knob_name = 'on_corr_k3sl_a45'
-
-
 
 tt = line.get_table()
 scale_multipole = np.zeros_like(tt.s)
@@ -35,28 +18,47 @@ scale_multipole[tt.rows.mask[r'mb.*']] = 1.0 # only bends as sources
 scale_multipole[tt.rows.mask[r'mc.*']] = 1.0 # all magnets called mcXXX used as correctors
 scale_multipole[tt.rows.mask[r'mss.*']] = 1.0 # all magnets called mssXXX used as correctors
 
-# Usage:
-rdt_contrib = IntegralCorrection(
-                         line=env['lhcb1'],
-                         tw=tw,
-                         feed_down=True,
-                         start=start,
-                         end=end,
-                         correction_knobs=correction_knobs,
-                         multipole=multipole,
-                         ip=None,
-                         target_quantities=target_quantities,
-                         generated_knob_name='on_corr_k3_ip5',
-                         scale_multipole=scale_multipole)
-print("Original correction:")
-rdt_contrib.print_corrections()
 
-rdt_contrib.clear_corrections()
-opt = rdt_contrib.correct()
+# Let's have a look at the a3 (skew sextupole)
 
-print("Before setting the knob:")
-rdt_contrib.print_corrections()
+def chorm_coupling_integrand(tw, tt):
+    return tt['k2sl'] * tw.dx * np.sqrt(tw.betx * tw.bety)  * np.exp(1j*(tw.mux - tw.muy))
 
-env[opt.knob_name] = 1.0
-print("After setting the knob:")
-rdt_contrib.print_corrections()
+arcs = ['12', '23', '34', '45', '56', '67', '78', '81']
+
+for arc_name in arcs:
+    start = f's.ds.r{arc_name[0]}.{beam_name}'
+    end = f'e.ds.l{arc_name[1]}.{beam_name}'
+    correction_knobs = [f'kss.a{arc_name}{beam_name}']
+    generated_knob_name = f'on_corr_k2sl_a{arc_name}_local'
+    multipole = 'k2sl'
+    target_quantities={
+        'chrom_coupling_real': lambda tw, tt: chorm_coupling_integrand(tw, tt).real,
+        'chrom_coupling_imag': lambda tw, tt: chorm_coupling_integrand(tw, tt).imag
+    }
+
+    # Usage:
+    rdt_contrib = IntegralCorrection(
+                            line=env['lhcb1'],
+                            tw=tw,
+                            feed_down=True,
+                            start=start,
+                            end=end,
+                            correction_knobs=correction_knobs,
+                            multipole=multipole,
+                            ip=None,
+                            target_quantities=target_quantities,
+                            generated_knob_name=generated_knob_name,
+                            scale_multipole=scale_multipole)
+    print("Original correction:")
+    rdt_contrib.print_corrections()
+
+    rdt_contrib.clear_corrections()
+    opt = rdt_contrib.correct()
+
+    print("Before setting the knob:")
+    rdt_contrib.print_corrections()
+
+    env[opt.knob_name] = 1.0
+    print("After setting the knob:")
+    rdt_contrib.print_corrections()
