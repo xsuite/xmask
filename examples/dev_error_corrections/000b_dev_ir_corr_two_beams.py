9f365bf065e97fdd3b32dfd893f42f1effc6b4dd
giadarol <giovanni.iadarola@cern.ch>
2026-02-22 07:13:36 +0100

    Use dictionary for configuration

--- examples/dev_error_corrections/000b_dev_ir_corr_two_beams.py
+++ examples/dev_error_corrections/000b_dev_ir_corr_two_beams.py
@@ -9,12 +9,7 @@ tw_b1 = env_no_err.lhcb1.twiss4d(reverse=False) # Reference twiss
 tw_b2 = env_no_err.lhcb2.twiss4d(reverse=False) # Reference twiss
 
 
-# Normal sextupole correction
-# correction_knobs=['kcsx3.l5', 'kcsx3.r5']
-# multipole='k2l'
-# target_quantities={'c12': (1, 2, 'diff'), 'c21': (2, 1, 'diff')}
-
-# Normal octupole correction ip5
+# # Normal octupole correction ip5
 # generated_knob_name='on_corr_k3_ip5'
 # correction_knobs=['kcox3.l5', 'kcox3.r5']
 # multipole='k3l'
@@ -24,16 +19,47 @@ tw_b2 = env_no_err.lhcb2.twiss4d(reverse=False) # Reference twiss
 # range_b2 = 'dfxj.4r5', 'dfxj.4l5'
 # feed_down = False # to have same result as legacy
 
-# Normal sextupole correction ip5
-generated_knob_name='on_corr_k2_ip5'
-correction_knobs=['kcsx3.l5', 'kcsx3.r5']
-multipole='k2l'
-target_quantities_b1={'f1020_b1': 'f1020'}
-target_quantities_b2={'f1020_b2': 'f1020'}
-range_b1 = 'dfxj.4l5', 'dfxj.4r5'
-range_b2 = 'dfxj.4r5', 'dfxj.4l5'
-feed_down = False # to have same result as legacy
+# # Normal sextupole correction ip5
+# generated_knob_name='on_corr_k2_ip5'
+# correction_knobs=['kcsx3.l5', 'kcsx3.r5']
+# multipole='k2l'
+# target_quantities_b1={'f1020_b1': 'f1020'}
+# target_quantities_b2={'f1020_b2': 'f1020'}
+# range_b1 = 'dfxj.4l5', 'dfxj.4r5'
+# range_b2 = 'dfxj.4r5', 'dfxj.4l5'
+# feed_down = False # to have same result as legacy
 
+ir_corrections = {
+    'ip5': {'range_b1': ['dfxj.4l5', 'dfxj.4r5'],'range_b2': ['dfxj.4r5', 'dfxj.4l5'],
+            'corrections': {
+                'on_corr_k2_ip5': {
+                    'correction_knobs': ['kcsx3.l5', 'kcsx3.r5'], 'multipole': 'k2l',
+                    'target_quantities_b1': {'f1020_b1': 'f1020'},
+                    'target_quantities_b2': {'f1020_b2': 'f1020'},
+                    'feed_down': False},
+                'on_corr_k3_ip5': {
+                    'correction_knobs': ['kcox3.l5', 'kcox3.r5'], 'multipole': 'k3l',
+                    'target_quantities_b1': {'f4000_b1': 'f4000'},
+                    'target_quantities_b2': {'f4000_b2': 'f4000'},
+                    'feed_down': False}
+        }
+    }
+}
+
+ip_name = 'ip5'
+correction_name = 'on_corr_k2_ip5'
+
+ip_corrections = ir_corrections[ip_name]
+range_b1 = ip_corrections['range_b1']
+range_b2 = ip_corrections['range_b2']
+corrections = ip_corrections['corrections']
+correction = corrections[correction_name]
+correction_knobs = correction['correction_knobs']
+multipole = correction['multipole']
+target_quantities_b1 = correction['target_quantities_b1']
+target_quantities_b2 = correction['target_quantities_b2']
+feed_down = correction['feed_down']
+generated_knob_name = correction_name
 
 # Usage:
 rdt_contrib_b1 = IntegralCorrection(
