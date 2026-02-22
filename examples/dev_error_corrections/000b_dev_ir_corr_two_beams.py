a7028eabe86c47e7f757df8e78c18c7d66ddb739
giadarol <giovanni.iadarola@cern.ch>
2026-02-22 07:21:32 +0100

    k4_ip5

--- examples/dev_error_corrections/000b_dev_ir_corr_two_beams.py
+++ examples/dev_error_corrections/000b_dev_ir_corr_two_beams.py
@@ -8,6 +8,9 @@ env_no_err = xt.load('collider_00_from_mad_no_errors.json')
 tw_b1 = env_no_err.lhcb1.twiss4d(reverse=False) # Reference twiss
 tw_b2 = env_no_err.lhcb2.twiss4d(reverse=False) # Reference twiss
 
+tt0_b1 = env.lhcb1.get_table(attr=True)
+tt0_b2 = env.lhcb2.get_table(attr=True)
+
 
 # # Normal octupole correction ip5
 # generated_knob_name='on_corr_k3_ip5'
@@ -41,13 +44,23 @@ ir_corrections = {
                     'correction_knobs': ['kcox3.l5', 'kcox3.r5'], 'multipole': 'k3l',
                     'target_quantities_b1': {'f4000_b1': 'f4000'},
                     'target_quantities_b2': {'f4000_b2': 'f4000'},
-                    'feed_down': False}
+                    'feed_down': False},
+                'on_corr_k4_ip5': {
+                    'correction_knobs': ['kcdx3.l5', 'kcdx3.r5'], 'multipole': 'k4l',
+                    'target_quantities_b1': {'f5000_b1': 'f5000'},
+                    'target_quantities_b2': {'f5000_b2': 'f5000'},
+                    'feed_down': False},
+                'on_corr_k1s_ip5': {
+                    'correction_knobs': ['kqsx3.l5', 'kqsx3.r5'], 'multipole': 'k1sl',
+                    'target_quantities_b1': {'f1001_b1': 'f1001'},
+                    'target_quantities_b2': {'f1001_b2': 'f1001'},
+                    'feed_down': False},
         }
     }
 }
 
 ip_name = 'ip5'
-correction_name = 'on_corr_k2_ip5'
+correction_name = 'on_corr_k4_ip5'
 
 ip_corrections = ir_corrections[ip_name]
 range_b1 = ip_corrections['range_b1']
@@ -86,17 +99,8 @@ rdt_contrib_b2 = IntegralCorrection(
                          target_quantities=target_quantities_b2,
                          generated_knob_name=generated_knob_name)
 
-# knob_opt_b1 = rdt_contrib_b1.get_optimizer()
-# knob_opt_b2 = rdt_contrib_b2.get_optimizer()
-
-# combined_opt = knob_opt_b1.opt.clone(add_targets=knob_opt_b2.opt.targets)
-# combined_opt.step()
-
-# knob_opt_b1.generate_knob()
-
 print("Original correction:")
 rdt_contrib_b1.print_corrections()
-
 rdt_contrib_b1.clear_corrections()
 
 knob_opt_b1 = rdt_contrib_b1.get_optimizer()
@@ -107,8 +111,6 @@ combined_opt = knob_opt_b1.opt.clone(add_targets=knob_opt_b2.opt.targets)
 combined_opt.step()
 knob_opt_b1.generate_knob()
 
-
-
 print("Before setting the knob:")
 rdt_contrib_b1.print_corrections()
 
@@ -116,20 +118,3 @@ env[knob_opt_b1.knob_name] = 1.0
 print("After setting the knob:")
 rdt_contrib_b1.print_corrections()
 
-# oo = rdt_contrib.run()
-# integrand_rdt = oo['(0, 4)_integrand_rdt']
-# integrand_loc = oo['(0, 4)_integrand_loc']
-# s = oo['s']
-# integrand_rdt *= np.exp(-1j * np.angle(integrand_rdt[0]))
-
-# import matplotlib.pyplot as plt
-# plt.close('all')
-# plt.figure(1)
-# plt.plot(s, integrand_loc/np.max(np.abs(integrand_loc)), label='loc real')
-# plt.plot(s, -integrand_rdt.real/np.max(np.abs(integrand_rdt)), label='rdt real')
-# plt.plot(s, -integrand_rdt.imag/np.max(np.abs(integrand_rdt)), label='rdt imag')
-# plt.plot(s, np.abs(integrand_rdt)/np.max(np.abs(integrand_rdt)), label='rdt abs')
-# plt.legend()
-# plt.title('Integrand for f0040')
-
-# plt.show()
