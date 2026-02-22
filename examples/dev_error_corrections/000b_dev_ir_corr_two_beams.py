84f64b7b414b6dba84563f4823283914bd8a9ed5
giadarol <giovanni.iadarola@cern.ch>
2026-02-22 10:55:52 +0100

    Correct a6 ip5

--- examples/dev_error_corrections/000b_dev_ir_corr_two_beams.py
+++ examples/dev_error_corrections/000b_dev_ir_corr_two_beams.py
@@ -75,12 +75,17 @@ ir_corrections = {
                     'target_quantities_b1': {'f0050_b1': 'f0050'},
                     'target_quantities_b2': {'f0050_b2': 'f0050'},
                     'feed_down': False},
+                'on_corr_k5s_ip5': {
+                    'correction_knobs': ['kctsx3.l5', 'kctsx3.r5'], 'multipole': 'k5sl',
+                    'target_quantities_b1': {'f1050_b1': 'f1050'},
+                    'target_quantities_b2': {'f1050_b2': 'f1050'},
+                    'feed_down': False}
         }
     }
 }
 
 ip_name = 'ip5'
-correction_name = 'on_corr_k4s_ip5'
+correction_name = 'on_corr_k5s_ip5'
 
 ip_corrections = ir_corrections[ip_name]
 range_b1 = ip_corrections['range_b1']
