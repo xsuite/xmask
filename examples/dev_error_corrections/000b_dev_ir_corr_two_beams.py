59f393161588cbe575656717bd3d24ca8edf96a3
giadarol <giovanni.iadarola@cern.ch>
2026-02-22 17:24:09 +0100

    ip1 also

--- examples/dev_error_corrections/000b_dev_ir_corr_two_beams.py
+++ examples/dev_error_corrections/000b_dev_ir_corr_two_beams.py
@@ -12,27 +12,56 @@ tt0_b1 = env.lhcb1.get_table(attr=True)
 tt0_b2 = env.lhcb2.get_table(attr=True)
 
 
-# # Normal octupole correction ip5
-# generated_knob_name='on_corr_k3_ip5'
-# correction_knobs=['kcox3.l5', 'kcox3.r5']
-# multipole='k3l'
-# target_quantities_b1={'f4000_b1': 'f4000'}
-# target_quantities_b2={'f4000_b2': 'f4000'}
-# range_b1 = 'dfxj.4l5', 'dfxj.4r5'
-# range_b2 = 'dfxj.4r5', 'dfxj.4l5'
-# feed_down = False # to have same result as legacy
-
-# # Normal sextupole correction ip5
-# generated_knob_name='on_corr_k2_ip5'
-# correction_knobs=['kcsx3.l5', 'kcsx3.r5']
-# multipole='k2l'
-# target_quantities_b1={'f1020_b1': 'f1020'}
-# target_quantities_b2={'f1020_b2': 'f1020'}
-# range_b1 = 'dfxj.4l5', 'dfxj.4r5'
-# range_b2 = 'dfxj.4r5', 'dfxj.4l5'
-# feed_down = False # to have same result as legacy
-
 ir_corrections = {
+    'ip1': {'range_b1': ['dfxj.4l1', 'dfxj.4r1'],'range_b2': ['dfxj.4r1', 'dfxj.4l1'],
+            'corrections': {
+                'on_corr_k2_ip1': {
+                    'correction_knobs': ['kcsx3.l1', 'kcsx3.r1'], 'multipole': 'k2l',
+                    'target_quantities_b1': {'f1020_b1': 'f1020'},
+                    'target_quantities_b2': {'f1020_b2': 'f1020'},
+                    'feed_down': False},
+                'on_corr_k3_ip1': {
+                    'correction_knobs': ['kcox3.l1', 'kcox3.r1'], 'multipole': 'k3l',
+                    'target_quantities_b1': {'f4000_b1': 'f4000'},
+                    'target_quantities_b2': {'f4000_b2': 'f4000'},
+                    'feed_down': False},
+                'on_corr_k4_ip1': {
+                    'correction_knobs': ['kcdx3.l1', 'kcdx3.r1'], 'multipole': 'k4l',
+                    'target_quantities_b1': {'f5000_b1': 'f5000'},
+                    'target_quantities_b2': {'f5000_b2': 'f5000'},
+                    'feed_down': False},
+                'on_corr_k5_ip1': {
+                    'correction_knobs': ['kctx3.l1', 'kctx3.r1'], 'multipole': 'k5l',
+                    'target_quantities_b1': {'f6000_b1': 'f6000'},
+                    'target_quantities_b2': {'f6000_b2': 'f6000'},
+                    'feed_down': False},
+                'on_corr_k1s_ip1': {
+                    'correction_knobs': ['kqsx3.l1', 'kqsx3.r1'], 'multipole': 'k1sl',
+                    'target_quantities_b1': {'f1001_b1': 'f1001'},
+                    'target_quantities_b2': {'f1001_b2': 'f1001'},
+                    'feed_down': False},
+                'on_corr_k2s_ip1': {
+                    'correction_knobs': ['kcssx3.l1', 'kcssx3.r1'], 'multipole': 'k2sl',
+                    'target_quantities_b1': {'f0030_b1': 'f0030'},
+                    'target_quantities_b2': {'f0030_b2': 'f0030'},
+                    'feed_down': False},
+                'on_corr_k3s_ip1': {
+                    'correction_knobs': ['kcosx3.l1', 'kcosx3.r1'], 'multipole': 'k3sl',
+                    'target_quantities_b1': {'f1030_b1': 'f1030'},
+                    'target_quantities_b2': {'f1030_b2': 'f1030'},
+                    'feed_down': False},
+                'on_corr_k4s_ip1': {
+                    'correction_knobs': ['kcdsx3.l1', 'kcdsx3.r1'], 'multipole': 'k4sl',
+                    'target_quantities_b1': {'f0050_b1': 'f0050'},
+                    'target_quantities_b2': {'f0050_b2': 'f0050'},
+                    'feed_down': False},
+                'on_corr_k5s_ip1': {
+                    'correction_knobs': ['kctsx3.l1', 'kctsx3.r1'], 'multipole': 'k5sl',
+                    'target_quantities_b1': {'f1050_b1': 'f1050'},
+                    'target_quantities_b2': {'f1050_b2': 'f1050'},
+                    'feed_down': False}
+        }
+    },
     'ip5': {'range_b1': ['dfxj.4l5', 'dfxj.4r5'],'range_b2': ['dfxj.4r5', 'dfxj.4l5'],
             'corrections': {
                 'on_corr_k2_ip5': {
@@ -81,13 +110,17 @@ ir_corrections = {
                     'target_quantities_b2': {'f1050_b2': 'f1050'},
                     'feed_down': False}
         }
-    }
+    },
+
+
 }
 
 all_correction_knobs = []
+all_generated_knobs = []
 for ip_name, ip_corrections in ir_corrections.items():
     for correction_name, correction in ip_corrections['corrections'].items():
         all_correction_knobs += correction['correction_knobs']
+        all_generated_knobs.append(correction_name)
 
 original_values = {kk: env[kk] for kk in all_correction_knobs}
 
@@ -95,61 +128,63 @@ original_values = {kk: env[kk] for kk in all_correction_knobs}
 for kk in all_correction_knobs:
     env[kk] = 0.0
 
-ip_name = 'ip5'
-correction_name = 'on_corr_k5s_ip5'
+for ip_name, ip_corrections in ir_corrections.items():
+    ip_corrections = ir_corrections[ip_name]
+    range_b1 = ip_corrections['range_b1']
+    range_b2 = ip_corrections['range_b2']
+    for correction_name, correction in ip_corrections['corrections'].items():
 
-ip_corrections = ir_corrections[ip_name]
-range_b1 = ip_corrections['range_b1']
-range_b2 = ip_corrections['range_b2']
-corrections = ip_corrections['corrections']
-correction = corrections[correction_name]
-correction_knobs = correction['correction_knobs']
-multipole = correction['multipole']
-target_quantities_b1 = correction['target_quantities_b1']
-target_quantities_b2 = correction['target_quantities_b2']
-feed_down = correction['feed_down']
-generated_knob_name = correction_name
+        correction_knobs = correction['correction_knobs']
+        multipole = correction['multipole']
+        target_quantities_b1 = correction['target_quantities_b1']
+        target_quantities_b2 = correction['target_quantities_b2']
+        feed_down = correction['feed_down']
+        generated_knob_name = correction_name
 
-# Usage:
-rdt_contrib_b1 = IntegralCorrection(
-                         line=env['lhcb1'],
-                         tw=tw_b1,
-                         start=range_b1[0],
-                         end=range_b1[1],
-                         correction_knobs=correction_knobs,
-                         multipole=multipole,
-                         ip=None, # not needed when RDT are used
-                         feed_down=feed_down,
-                         target_quantities=target_quantities_b1,
-                         generated_knob_name=generated_knob_name)
+        # Usage:
+        rdt_contrib_b1 = IntegralCorrection(
+                                line=env['lhcb1'],
+                                tw=tw_b1,
+                                start=range_b1[0],
+                                end=range_b1[1],
+                                correction_knobs=correction_knobs,
+                                multipole=multipole,
+                                ip=None, # not needed when RDT are used
+                                feed_down=feed_down,
+                                target_quantities=target_quantities_b1,
+                                generated_knob_name=generated_knob_name)
 
-rdt_contrib_b2 = IntegralCorrection(
-                         line=env['lhcb2'],
-                         tw=tw_b2,
-                         start=range_b2[0],
-                         end=range_b2[1],
-                         correction_knobs=[], # only targets here
-                         multipole=multipole,
-                         ip=None, # not needed when RDT are used
-                         feed_down=feed_down,
-                         target_quantities=target_quantities_b2,
-                         generated_knob_name=generated_knob_name)
+        rdt_contrib_b2 = IntegralCorrection(
+                                line=env['lhcb2'],
+                                tw=tw_b2,
+                                start=range_b2[0],
+                                end=range_b2[1],
+                                correction_knobs=[], # only targets here
+                                multipole=multipole,
+                                ip=None, # not needed when RDT are used
+                                feed_down=feed_down,
+                                target_quantities=target_quantities_b2,
+                                generated_knob_name=generated_knob_name)
 
-knob_opt_b1 = rdt_contrib_b1.get_optimizer()
-knob_opt_b2 = rdt_contrib_b2.get_optimizer()
+        knob_opt_b1 = rdt_contrib_b1.get_optimizer()
+        knob_opt_b2 = rdt_contrib_b2.get_optimizer()
 
-combined_opt = knob_opt_b1.opt.clone(add_targets=knob_opt_b2.opt.targets)
-combined_opt.step()
-knob_opt_b1.generate_knob()
+        combined_opt = knob_opt_b1.opt.clone(add_targets=knob_opt_b2.opt.targets)
+        combined_opt.step()
+        knob_opt_b1.generate_knob()
 
-print("Before setting the knob:")
-rdt_contrib_b1.print_corrections()
+# assert all generated knobs are off and all correction knobs are off
+for kk in all_correction_knobs:
+    assert env[kk] == 0.0, f"Correction knob {kk} is not zero at the end of the test"
 
-env[knob_opt_b1.knob_name] = 1.0
-print("After setting the knob:")
-rdt_contrib_b1.print_corrections()
+for kk in all_generated_knobs:
+    assert env[kk] == 0.0, f"Generated knob {kk} is not zero at the end of the test"
 
-print("Original values:")
-for kk in correction_knobs:
-    value = original_values[kk]
-    print(f"{kk}: {value}")
+# switch on all generated knobs
+for kk in all_generated_knobs:
+    env[kk] = 1.0
+
+# check similarity with original values
+import xobjects as xo
+for nn, vv in original_values.items():
+    xo.assert_allclose(env[nn], vv, rtol=8e-2, atol=1e-12)
