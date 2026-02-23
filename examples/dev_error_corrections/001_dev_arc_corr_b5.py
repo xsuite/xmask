3d7fadd6cb82fe4cc7e374ae784445f56aa34b09
giadarol <giovanni.iadarola@cern.ch>
2026-02-23 06:38:00 +0100

    A fix for b2

--- examples/dev_error_corrections/001_dev_arc_corr_b5.py
+++ examples/dev_error_corrections/001_dev_arc_corr_b5.py
@@ -15,12 +15,13 @@ tw_b12 = {'b1': tw_b1, 'b2': tw_b2}
 # Let's have a look at the b5 (k4) # decapole
 
 arc_multipoles_to_suppress = {
-    'k2l': 'kcs',
-    'k3l': 'kco',
+    # 'k2l': 'kcs',
+    # 'k3l': 'kco',
     'k4l': 'kcd',
 }
 
 arcs = ['12', '23', '34', '45', '56', '67', '78', '81']
+arcs = ['45'] # for testing
 beams = ['b1', 'b2']
 
 for beam_name in beams:
@@ -28,14 +29,18 @@ for beam_name in beams:
     line = env[f'lhc{beam_name}']
     tw = tw_b12[beam_name]
 
-    tt = line.get_table()
+    tt = line.get_table(attr=True)
     scale_multipole = np.zeros_like(tt.s)
     scale_multipole[tt.rows.mask[r'mb.*']] = 1.0 # only bends as sources
     scale_multipole[tt.rows.mask[r'mc.*']] = 1.0 # all magnets called mcXXX used as correctors
 
     for arc_name in arcs:
-        start = f's.ds.r{arc_name[0]}.{beam_name}'
-        end = f'e.ds.l{arc_name[1]}.{beam_name}'
+        if beam_name == 'b1':
+            start = f's.ds.r{arc_name[0]}.{beam_name}'
+            end = f'e.ds.l{arc_name[1]}.{beam_name}'
+        else:
+            start = f'e.ds.l{arc_name[1]}.{beam_name}'
+            end = f's.ds.r{arc_name[0]}.{beam_name}'
         for multipole, knob_prefix in arc_multipoles_to_suppress.items():
             correction_knobs = [f'{knob_prefix}.a{arc_name}{beam_name}']
             target_quantities={'multipole_to_suppress': lambda tw, tt: tt[multipole].sum()}
