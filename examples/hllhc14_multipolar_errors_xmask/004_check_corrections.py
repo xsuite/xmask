6646a442191a96e7ec1fe1dc91cb7e2d4278b7c9
giadarol <giovanni.iadarola@cern.ch>
2026-03-12 16:16:13 +0100

    Check on spool pieces and b2

--- examples/hllhc14_multipolar_errors_xmask/004_check_corrections.py
+++ examples/hllhc14_multipolar_errors_xmask/004_check_corrections.py
@@ -1,12 +1,30 @@
 import xtrack as xt
 import matplotlib.pyplot as plt
 import numpy as np
+import xobjects as xo
 
 env_test = xt.load('lhc_arc_errors_with_correction.json')
 env_ref = xt.load('../hllhc14_multipolar_errors_legacy/'
                    'collider_errors_on_corrections_on.json')
 
-line_name = 'lhcb1'
+# Check spool pieces
+tt_kcs_test = env_test.vars.get_table().rows.match(r'kcs\..*').rows.match_not('.*_from_.*')
+tt_kcs_ref = env_ref.vars.get_table().rows.match(r'kcs\..*').rows.match_not('.*_from_.*')
+assert np.all(tt_kcs_test.name == tt_kcs_ref.name)
+xo.assert_allclose(tt_kcs_test.value, tt_kcs_ref.value, rtol=0.02)
+
+tt_kco_test = env_test.vars.get_table().rows.match(r'kco\..*').rows.match_not('.*_from_.*')
+tt_kco_ref = env_ref.vars.get_table().rows.match(r'kco\..*').rows.match_not('.*_from_.*')
+assert np.all(tt_kco_test.name == tt_kco_ref.name)
+xo.assert_allclose(tt_kco_test.value, tt_kco_ref.value, rtol=0.001)
+
+tt_kcd_test = env_test.vars.get_table().rows.match(r'kcd\..*').rows.match_not('.*_from_.*')
+tt_kcd_ref = env_ref.vars.get_table().rows.match(r'kcd\..*').rows.match_not('.*_from_.*')
+assert np.all(tt_kcd_test.name == tt_kcd_ref.name)
+xo.assert_allclose(tt_kcd_test.value, tt_kcd_ref.value, rtol=0.001,
+                   atol=0.03*np.max(np.abs(tt_kcd_ref.value)))
+
+line_name = 'lhcb2'
 
 line_test = env_test[line_name]
 line_ref = env_ref[line_name]
@@ -16,8 +34,12 @@ tt_test = line_test.get_table(attr=True)
 
 # kill IR non-linearities in reference machine (for comparison)
 for ipn in [1, 2, 5, 8]:
-    start_ir = f'e.ds.l{ipn}.b1'
-    end_ir = f's.ds.r{ipn}.b1'
+    if line_name == 'lhcb1':
+        start_ir = f'e.ds.l{ipn}.b1'
+        end_ir = f's.ds.r{ipn}.b1'
+    else:
+        start_ir = f's.ds.r{ipn}.b2'
+        end_ir = f'e.ds.l{ipn}.b2'
     tt = line_ref.get_table()
     tt_ir = tt.rows[start_ir:end_ir]
     for nn in tt_ir.name:
@@ -36,8 +58,12 @@ for ttww in [tw_test, tw_ref]:
 chrom_coupling_integ_test = {}
 chrom_coupling_integ_ref = {}
 for arc in ['12', '23', '34', '45', '56', '67', '78', '81']:
-    start = f's.ds.r{arc[0]}.b1'
-    end = f'e.ds.l{arc[1]}.b1'
+    if line_name == 'lhcb1':
+        start = f's.ds.r{arc[0]}.b1'
+        end = f'e.ds.l{arc[1]}.b1'
+    else:
+        start = f'e.ds.l{arc[1]}.b2'
+        end = f's.ds.r{arc[0]}.b2'
     tt_test_arc = tw_test.rows[start:end]
     tt_ref_arc = tw_ref.rows[start:end]
 
@@ -54,6 +80,7 @@ tt_on_corr = env_test.vars.get_table().rows['on_corr_k2sl.*']
 assert len(tt_on_corr) == 9
 env_test.set(tt_on_corr.name, 0)
 nlchr_test_no_corr = line_test.get_non_linear_chromaticity(num_delta=20)
+env_test.set(tt_on_corr.name, 1)
 
 
 plt.close('all')
