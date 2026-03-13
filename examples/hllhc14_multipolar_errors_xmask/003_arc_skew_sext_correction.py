ffaec7294c90cd8dfc572690df2def7be6da4020
giadarol <giovanni.iadarola@cern.ch>
2026-03-13 13:44:19 +0100

    Works also on skew quadrupoles

--- examples/hllhc14_multipolar_errors_xmask/003_arc_skew_sext_correction.py
+++ examples/hllhc14_multipolar_errors_xmask/003_arc_skew_sext_correction.py
@@ -1,22 +1,35 @@
-
 import numpy as np
 import xtrack as xt
 from integral_correction import IntegralCorrection
 
+env = xt.load('lhc_arc_errors_with_spool_piece_corrections.json')
+
 arc_names = ['12', '23', '34', '45', '56', '67', '78', '81']
 
-generated_knob_prefix = 'on_corr_k2sl'
-multipole = 'k2sl'
-def chrom_coupling_integrand(tw, tt):
-    return tt['k2sl'] * tw.dx * np.sqrt(tw.betx * tw.bety) * np.exp(1j*2*np.pi*(tw.mux - tw.muy))
-target_integrand = chrom_coupling_integrand
+# generated_knob_prefix = 'on_corr_k2sl'
+# multipole = 'k2sl'
+# def chrom_coupling_integrand(tw, tt):
+#     return tt['k2sl'] * tw.dx * np.sqrt(tw.betx * tw.bety) * np.exp(1j*2*np.pi*(tw.mux - tw.muy))
+# target_integrand = chrom_coupling_integrand
+# correction_knobs = {
+#     'b1': {arc: [f'kss.a{arc}b1'] for arc in arc_names},
+#     'b2': {arc: [f'kss.a{arc}b2'] for arc in arc_names},
+# }
+
+generated_knob_prefix = 'on_corr_k1sl'
+multipole = 'k1sl'
+target_integrand = 'f1001'
 correction_knobs = {
-    'b1': {arc: [f'kss.a{arc}b1'] for arc in arc_names},
-    'b2': {arc: [f'kss.a{arc}b2'] for arc in arc_names},
+    'b1': {'12': ['kqs.r1b1', 'kqs.l2b1'], '23': ['kqs.a23b1'],
+           '34': ['kqs.r3b1', 'kqs.l4b1'], '45': ['kqs.a45b1'],
+           '56': ['kqs.r5b1', 'kqs.l6b1'], '67': ['kqs.a67b1'],
+           '78': ['kqs.r7b1', 'kqs.l8b1'], '81': ['kqs.a81b1']},
+    'b2': {'12': ['kqs.a12b2'], '23': ['kqs.r2b2', 'kqs.l3b2'],
+           '34': ['kqs.a34b2'], '45': ['kqs.r4b2', 'kqs.l5b2'],
+           '56': ['kqs.a56b2'], '67': ['kqs.r6b2', 'kqs.l7b2'],
+           '78': ['kqs.a78b2'], '81': ['kqs.r8b2', 'kqs.l1b2']},
 }
-
-
-env = xt.load('lhc_arc_errors_with_spool_piece_corrections.json')
+env['on_error_arc_k1s'] = 1.0
 
 # Status of error and correction knobs
 tt_err_knobs = env.vars.get_table().rows[r'on_error_arc.*|on_corr_.*']
@@ -43,8 +56,17 @@ for beam_name in ['b1', 'b2']:
     scale_multipole[tt.rows.mask[r'mb.*']] = 1.0 # only bends as sources
     scale_multipole[tt.rows.mask[r'mc.*']] = 1.0 # all magnets called mcXXX used as correctors
     scale_multipole[tt.rows.mask[r'mss.*']] = 1.0 # all magnets called mssXXX used as correctors
+    scale_multipole[tt.rows.mask[r'mqs.*']] = 1.0 # all magnets called mqsXXX used as correctors
 
     # Function that we want to minimize
+    if isinstance(target_integrand, str):
+        target_quantities = {'target': target_integrand}
+    else:
+        # Assume a complex callable
+        target_quantities={
+            'target_real': lambda tw, tt: np.real(target_integrand(tw, tt)),
+            'target_imag': lambda tw, tt: np.imag(target_integrand(tw, tt))
+        }
 
 
     # Global correction setup (we run it after local)
@@ -54,10 +76,6 @@ for beam_name in ['b1', 'b2']:
     for arc in arc_names:
         correction_knobs_global += correction_knobs[beam_name][arc]
     generated_knob_name = f'{generated_knob_prefix}_global'
-    target_quantities={
-        'target_real': lambda tw, tt: np.real(target_integrand(tw, tt)),
-        'target_imag': lambda tw, tt: np.imag(target_integrand(tw, tt))
-    }
 
     # Create calculator for global correction (not run)
     rdt_contrib_glob = IntegralCorrection(
@@ -84,10 +102,6 @@ for beam_name in ['b1', 'b2']:
             end = f's.ds.r{arc_name[0]}.b2'
         correction_knobs_local = correction_knobs[beam_name][arc_name]
         generated_knob_name = f'{generated_knob_prefix}_a{arc_name}_local'
-        target_quantities={
-            'target_real': lambda tw, tt: np.real(target_integrand(tw, tt)),
-            'target_imag': lambda tw, tt: np.imag(target_integrand(tw, tt))
-        }
 
         # Usage:
         arc_integ = IntegralCorrection(
