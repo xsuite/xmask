f0ef5360678862409130496fea685dcffc3dd4e9
giadarol <giovanni.iadarola@cern.ch>
2026-03-14 09:31:24 +0100

    This passes

--- examples/hllhc14_multipolar_errors_xmask/001_check.py
+++ examples/hllhc14_multipolar_errors_xmask/001_check.py
@@ -12,6 +12,7 @@ env_test['on_error_arc_k0s'] = 0
 env_test['on_error_arc_k1'] = 0
 env_test['on_error_arc_k1s'] = 0
 
+issues = []
 for line_to_check in ['lhcb1', 'lhcb2']:
     line_test = env_test[line_to_check]
     line_ref = env_ref[line_to_check]
@@ -45,12 +46,15 @@ for line_to_check in ['lhcb1', 'lhcb2']:
                       'ms.28l3.b2', 'ms.32l3.b2', 'ms.34l3.b2', 'ms.30r2.b2',
                       'ms.29l4.b2', 'ms.33l4.b2', 'ms.33r3.b2', 'ms.29r3.b2',
                       'ms.28l5.b2', 'ms.32l5.b2', 'ms.34l5.b2', 'ms.30r4.b2',
-                      'ms.29l6.b2', 'ms.33l6.b2', 'ms.33r5.b2'}:
+                      'ms.29l6.b2', 'ms.33l6.b2', 'ms.33r5.b2', 'ms.29r5.b2',
+                      'ms.28l7.b2', 'ms.32l7.b2', 'ms.34l7.b2', 'ms.30r6.b2',
+                      'ms.29l8.b2', 'ms.33l8.b2', 'ms.33r7.b2', 'ms.29r7.b2',
+                      'ms.28l1.b2', 'ms.32l1.b2', 'ms.34l1.b2', 'ms.30r8.b2'}:
                 # issue in the legacy macros, errors non applied to this magnet
                 assert np.all(line_ref[nn].get_total_knl_ksl()[0][3:] == 0)
                 assert np.all(line_ref[nn].get_total_knl_ksl()[1] == 0)
                 continue
-            if nn in {'mco.b30r3.b2'}:
+            if nn in {'mco.b30r3.b2', 'mco.11l1.b2', 'mco.12l1.b2'}:
                 "magnet seems to be off"
                 assert np.all(line_ref[nn].knl == 0)
                 assert np.all(line_ref[nn].ksl == 0)
@@ -58,10 +62,9 @@ for line_to_check in ['lhcb1', 'lhcb2']:
                 assert np.all(line_test[nn].ksl == 0)
                 continue
 
-
             if hasattr(line_ref[nn], 'knl'):
+                knl_tot_nn, ksl_tot_nn = line_test[nn].get_total_knl_ksl()
                 for ii in range(len(line_ref[nn].knl)):
-                    knl_tot_nn, ksl_tot_nn = line_test[nn].get_total_knl_ksl()
                     xo.assert_allclose(knl_tot_nn[ii], line_ref[nn].knl[ii],
                                     rtol=1e-10, atol=1e-10)
                     xo.assert_allclose(ksl_tot_nn[ii], line_ref[nn].ksl[ii],
