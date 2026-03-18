131ce510f1f846c513a7a2b403e0369be4a1ef74
giadarol <giovanni.iadarola@cern.ch>
2026-03-18 21:36:44 +0100

    Check b1/b2 for common magnets

--- examples/hllhc14_multipolar_errors_xmask/001b_check_triplets.py
+++ examples/hllhc14_multipolar_errors_xmask/001b_check_triplets.py
@@ -2,17 +2,22 @@ import xtrack as xt
 import xobjects as xo
 import numpy as np
 
+# env_test = xt.load('lhc_arc_errors_arc_and_triplets15.json')
+# line_test = env_test['lhcb1']
+# line_ref = xt.load(
+#     '../../../20260318_andrea_fornara_errors/'
+#     'sorting/triplet_errors/sorting/full_vertical_data_sorting/results/'
+#     'line_lhcb1_perm6.json'
+# )
+
 env_test = xt.load('lhc_arc_errors_arc_and_triplets15.json')
-line_b1_ref = xt.load(
+line_test = env_test['lhcb2']
+line_ref = xt.load(
     '../../../20260318_andrea_fornara_errors/'
     'sorting/triplet_errors/sorting/full_vertical_data_sorting/results/'
-    'line_lhcb1_perm6.json'
+    'line_lhcb2_perm6.json'
 )
 
-line_b1 = env_test['lhcb1']
-
-line_test = env_test['lhcb1']
-line_ref = line_b1_ref
 
 line_test.cycle('ip7')
 line_ref.cycle('ip7')
--- examples/hllhc14_multipolar_errors_xmask/load_wise.py
+++ examples/hllhc14_multipolar_errors_xmask/load_wise.py
@@ -1,4 +1,5 @@
 import xtrack as xt
+import xobjects as xo
 import numpy as np
 from scipy.special import factorial
 
@@ -204,3 +205,13 @@ def set_multipole_errors_in_line(line, multipole_errors,
                 line.ref[nn].knl_rel[ii] = kknn_rel * ref_knob_kn
                 line.ref[nn].ksl_rel[ii] = kkss_rel * ref_knob_ks
 
+def assert_are_same_multipoles_b1_b2(ele_b1, ele_b2, atol=0, rtol=0):
+    knl_b1, ksl_b1 = ele_b1.get_total_knl_ksl()
+    knl_b2, ksl_b2 = ele_b2.get_total_knl_ksl()
+    knl_b2_check = knl_b2.copy()
+    ksl_b2_check = ksl_b2.copy()
+    knl_b2_check[1::2] *= -1
+    ksl_b2_check[0::2] *= -1
+
+    xo.assert_allclose(knl_b1, knl_b2_check, rtol=rtol, atol=atol)
+    xo.assert_allclose(ksl_b1, ksl_b2_check, rtol=rtol, atol=atol)
