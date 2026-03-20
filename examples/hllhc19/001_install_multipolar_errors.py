77692922e89b6742c4c5ed058d7f31b4aa74b67a
giadarol <giovanni.iadarola@cern.ch>
2026-03-20 17:56:13 +0100

    Knobs from the beginning

--- examples/hllhc19/001_install_multipolar_errors.py
+++ examples/hllhc19/001_install_multipolar_errors.py
@@ -11,9 +11,9 @@ apply_multipolar_errors_config = config['apply_multipolar_errors']
 
 if apply_multipolar_errors_config:
     # Read the configuration from the yaml
-    config = apply_multipolar_errors_config.pop('_config_')
-    min_order = config['min_order']
-    max_order = config['max_order']
+    err_conf = apply_multipolar_errors_config.pop('_config_')
+    min_order = err_conf['min_order']
+    max_order = err_conf['max_order']
 
     # Apply the errors
     for knob_name, json_file in apply_multipolar_errors_config.items():
@@ -27,5 +27,10 @@ if apply_multipolar_errors_config:
                                     min_order=min_order, max_order=max_order,
                                     error_knob_name=knob_name,
                                     append_order_to_knob_name=True)
+
+# Force the knobs settings (on_error_... might be forced to 1 by the error,
+# installation and we want the user setting, it present to be applied on top of that)
+for knob_name, knob_value in config['knob_settings'].items():
+    lhc[knob_name] = knob_value
 
 lhc.to_json('collider_01_multipolar_errors.json')
