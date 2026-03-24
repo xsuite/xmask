03d499b60f56ee0f2561b4e84b9580b63b4093e9
giadarol <giovanni.iadarola@cern.ch>
2026-03-24 19:17:57 +0100

    Clean up

--- examples/hllhc19/005_footprint.py
+++ examples/hllhc19/005_footprint.py
@@ -1,12 +1,12 @@
 import xtrack as xt
 
 
-collider = xt.Environment.from_json('./collider_04_tuned_and_leveled_bb_on.json')
-collider.build_trackers()
+lhc = xt.Environment.from_json('./collider_04_tuned_and_leveled_bb_on.json')
+lhc.build_trackers()
 
-fp_polar_no_rescale = collider['b1'].get_footprint(nemitt_x=2.5e-6, nemitt_y=2.5e-6)
+fp_polar_no_rescale = lhc['b1'].get_footprint(nemitt_x=2.5e-6, nemitt_y=2.5e-6)
 
-fp_polar_with_rescale = collider['b1'].get_footprint(
+fp_polar_with_rescale = lhc['b1'].get_footprint(
     nemitt_x=2.5e-6, nemitt_y=2.5e-6,
     linear_rescale_on_knobs=[
         xt.LinearRescale(knob_name='beambeam_scale', v0=0.0, dv=0.1)]
@@ -14,7 +14,7 @@ fp_polar_with_rescale = collider['b1'].get_footprint(
 
 #!end-doc-part
 
-fp_ua_with_rescale = collider['b1'].get_footprint(
+fp_ua_with_rescale = lhc['b1'].get_footprint(
     nemitt_x=2.5e-6, nemitt_y=2.5e-6,
     mode='uniform_action_grid',
     linear_rescale_on_knobs=[
