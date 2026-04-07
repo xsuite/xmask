f8a3ee5845afd28af855110805e3ba669ef0cf1f
giadarol <giovanni.iadarola@cern.ch>
2026-04-07 07:35:14 -0300

    Add reference file

--- tests/test_multipole_errors_and_correction.py
+++ tests/test_multipole_errors_and_correction.py
@@ -253,6 +253,37 @@ def test_multipole_errors_arcs_against_ref():
                         xo.assert_allclose(ksl_tot_nn[ii], line_ref[nn].ksl[ii],
                                             rtol=1e-10, atol=1e-10)
 
+def test_multipole_errors_ir_corrections_against_ref():
+
+    env = xt.load(test_data_dir /
+        'hllhc14_references_from_legacy/collider_errors_on_corrections_on.json')
+
+    # Reference twiss
+    env_no_err = xt.load('../hllhc14_multipolar_errors_legacy/collider_errors_off_corrections_off.json')
+    tw_b1 = env_no_err.lhcb1.twiss4d(reverse=False) # Reference twiss
+    tw_b2 = env_no_err.lhcb2.twiss4d(reverse=False) # Reference twiss
+
+    all_correction_knobs = []
+    all_generated_knobs = []
+    for ip_name, ip_corrections in IR_CORRECTIONS.items():
+        for correction_name, correction in ip_corrections['corrections'].items():
+            all_correction_knobs += correction['correction_knobs']
+            all_generated_knobs.append(correction_name)
+
+    original_values = {kk: env[kk] for kk in all_correction_knobs}
+
+    # Clean original values
+    for kk in all_correction_knobs:
+        env[kk] = 0.0
+
+    # Local correction of IR15 multipole errors
+    xmlhc.correct_ir_errors(env, twiss_b1=tw_b1, twiss_b2=tw_b2,
+                            corrections=IR_CORRECTIONS)
+
+    # check similarity with original values
+    import xobjects as xo
+    for nn, vv in original_values.items():
+        xo.assert_allclose(env[nn], vv, rtol=8e-2, atol=1e-12)
 
 
 # One possible configuration - final magnet-asset association still to be defined
