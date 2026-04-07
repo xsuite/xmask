d54808bdd1ff564a1fede5ffba5d455687422d41
giadarol <giovanni.iadarola@cern.ch>
2026-04-07 05:49:42 -0300

    Loading also the triplets

--- tests/test_multipole_errors_and_correction.py
+++ tests/test_multipole_errors_and_correction.py
@@ -5,15 +5,196 @@ from pathlib import Path
 
 test_data_dir = Path(__file__).parent.parent / "test_data"
 
-fname_rotations = (test_data_dir /
-    "hllhc19/prepare_multipolar_errors/multipole_errors_pre_hllhc/magnet_orientation.tab")
-fname_err_table = (test_data_dir /
-    "hllhc19/prepare_multipolar_errors/multipole_errors_pre_hllhc/collision_errors-emfqcs-6.tfs")
+def test_multipole_errors_and_correction():
 
-min_order = 0
-max_order = 15
+    ###################
+    # Load arc errors #
+    ###################
 
-multipole_errors_arcs, tt_err_arc = xmlhc.load_wise_table_arc_magnets(
-    fname_err_table=fname_err_table,
-    fname_rotations=fname_rotations,
-    min_order=min_order, max_order=max_order)
+    fname_rotations = (test_data_dir /
+        "hllhc19/prepare_multipolar_errors/multipole_errors_pre_hllhc/magnet_orientation.tab")
+    fname_err_table = (test_data_dir /
+        "hllhc19/prepare_multipolar_errors/multipole_errors_pre_hllhc/collision_errors-emfqcs-6.tfs")
+
+    min_order = 0
+    max_order = 15
+
+    multipole_errors_arcs, tt_err_arc = xmlhc.load_wise_table_arc_magnets(
+        fname_err_table=fname_err_table,
+        fname_rotations=fname_rotations,
+        min_order=min_order, max_order=max_order)
+
+    #############################
+    # Load inner triplet errors #
+    #############################
+
+    multipole_errors_triplets15 = {}
+    for nn in MAGNET_ASSET_ASSOCIATION:
+        asset_name = MAGNET_ASSET_ASSOCIATION[nn]
+        is_rot = IS_ROTATED[nn]
+
+        magnet_meas_data = load_hllhc_multipole_json(test_data_dir /
+            "hllhc19/prepare_multipolar_errors/multipole_errors_hllhc_ir15" /
+            DATA_FILES[asset_name])
+        ref_radius = magnet_meas_data.pop('ref_radius')
+
+        main_order = 1 # The are all quadrupoles
+
+        knl_rel_b1, ksl_rel_b1 = xmlhc.convert_multipolar_expansion(
+            magnet_meas_data=magnet_meas_data,
+            is_rotated=is_rot,
+            main_order=main_order,
+            ref_radius=ref_radius
+        )
+
+        knl_rel_b2, ksl_rel_b2 = xmlhc.convert_multipolar_expansion(
+            magnet_meas_data=magnet_meas_data,
+            is_rotated=not is_rot,
+            main_order=main_order,
+            ref_radius=ref_radius
+        )
+
+        multipole_errors_triplets15[nn + '/lhcb1'] = {'knl_rel': knl_rel_b1, 'ksl_rel': ksl_rel_b1}
+        multipole_errors_triplets15[nn + '/lhcb2'] = {'knl_rel': knl_rel_b2, 'ksl_rel': ksl_rel_b2}
+
+        order, is_skew = xmlhc.order_and_is_skew_from_name(nn)
+
+        multipole_errors_triplets15[nn+'/lhcb1']['main_order'] = order
+        multipole_errors_triplets15[nn+'/lhcb1']['main_is_skew'] = is_skew
+        multipole_errors_triplets15[nn+'/lhcb2']['main_order'] = order
+        multipole_errors_triplets15[nn+'/lhcb2']['main_is_skew'] = is_skew
+
+        # Alternative naming convention
+        multipole_errors_triplets15[nn + '/b1'] = multipole_errors_triplets15[nn + '/lhcb1']
+        multipole_errors_triplets15[nn + '/b2'] = multipole_errors_triplets15[nn + '/lhcb2']
+
+
+# One possible configuration - final magnet-asset association still to be defined
+MAGNET_ASSET_ASSOCIATION = {
+    'mqxfa.b3l1': 'mqxfa12b',
+    'mqxfa.a3l1': 'mqxfa17b',
+    'mqxfb.b2l1': 'mqxfb10',
+    'mqxfb.a2l1': 'mqxfb11',
+    'mqxfa.b1l1': 'mqxfa13b',
+    'mqxfa.a1l1': 'mqxfa18',
+    'mqxfa.a1r1': 'mqxfa07b',
+    'mqxfa.b1r1': 'mqxfa15',
+    'mqxfb.a2r1': 'mqxfb03',
+    'mqxfb.b2r1': 'mqxfb06',
+    'mqxfa.a3r1': 'mqxfa14b',
+    'mqxfa.b3r1': 'mqxfa08b',
+    'mqxfa.b3l5': 'mqxfa22',
+    'mqxfa.a3l5': 'mqxfa21',
+    'mqxfb.b2l5': 'mqxfb04',
+    'mqxfb.a2l5': 'mqxfb05',
+    'mqxfa.b1l5': 'mqxfa11',
+    'mqxfa.a1l5': 'mqxfa10',
+    'mqxfa.a1r5': 'mqxfa19',
+    'mqxfa.b1r5': 'mqxfa20',
+    'mqxfb.a2r5': 'mqxfb07',
+    'mqxfb.b2r5': 'mqxfb08',
+    'mqxfa.a3r5': 'mqxfa23',
+    'mqxfa.b3r5': 'mqxfa24',
+    'mbxf.4l1': 'mbxf2',
+    'mbxf.4r1': 'mbxf1',
+    'mbxf.4l5': 'mbxf5',
+    'mbxf.4r5': 'mbxf3',
+}
+
+DATA_FILES = {
+'mqxfa03':  'FQ_MQXFA/MQXFA03_CA01.json',
+'mqxfa04':  'FQ_MQXFA/MQXFA04_CA02.json',
+'mqxfa05':  'FQ_MQXFA/MQXFA05_CA03.json',
+'mqxfa06':  'FQ_MQXFA/MQXFA06_CA04.json',
+'mqxfa07b': 'FQ_MQXFA/MQXFA07b_CA01.json',
+'mqxfa08b': 'FQ_MQXFA/MQXFA08b_CA07.json',
+'mqxfa10':  'FQ_MQXFA/MQXFA10_CA05.json',
+'mqxfa11':  'FQ_MQXFA/MQXFA11_CA06.json',
+'mqxfa12b': 'FQ_MQXFA/MQXFA12b_CA05.json',
+'mqxfa13b': 'FQ_MQXFA/MQXFA13b_CA03.json',
+'mqxfa14b': 'FQ_MQXFA/MQXFA14b_CA08.json',
+'mqxfa15':  'FQ_MQXFA/MQXFA15_CA02.json',
+'mqxfa17b': 'FQ_MQXFA/MQXFA17b_CA06.json',
+'mqxfa18':  'FQ_MQXFA/MQXFA18_CA04.json',
+'mqxfa19':  'FQ_MQXFA/MQXFA19_CA07.json',
+'mqxfa20':  'FQ_MQXFA/MQXFA20_CA08.json',
+'mqxfa21':  'FQ_MQXFA/MQXFA21_CA09.json',
+'mqxfa22':  'FQ_MQXFA/MQXFA22_CA09.json',
+'mqxfa23':  'FQ_MQXFA/MQXFA23_CA10.json',
+'mqxfa24':  'FQ_MQXFA/MQXFA24_CA10.json',
+'mqxfb03':  'FQ_MQXFB/FQ_MQXFB03_cold_nominal.json',
+'mqxfb04':  'FQ_MQXFB/FQ_MQXFB04_cold_nominal.json',
+'mqxfb05':  'FQ_MQXFB/FQ_MQXFB05_cold_nominal.json',
+'mqxfb06':  'FQ_MQXFB/FQ_MQXFB06_cold_nominal.json',
+'mqxfb07':  'FQ_MQXFB/FQ_MQXFB07_cold_nominal.json',
+'mqxfb08':  'FQ_MQXFB/FQ_MQXFB08_cold_nominal.json',
+'mqxfb09':  'FQ_MQXFB/FQ_MQXFB09_cold_nominal.json',
+'mqxfb10':  'FQ_MQXFB/FQ_MQXFB10_cold_nominal.json',
+'mqxfb11':  'FQ_MQXFB/FQ_MQXFB11_cold_nominal.json',
+'mbxf1': 'FQ_MBXF/FQ_MBXF1_cold_nominal.json',
+'mbxf2': 'FQ_MBXF/FQ_MBXF2_cold_nominal.json',
+'mbxf3': 'FQ_MBXF/FQ_MBXF3_cold_nominal.json',
+'mbxf5': 'FQ_MBXF/FQ_MBXF5_cold_nominal.json',
+}
+
+
+IS_ROTATED = {
+    'mqxfa.b3l1': False,
+    'mqxfa.a3l1': True,
+    'mqxfb.b2l1': True,
+    'mqxfb.a2l1': False,
+    'mqxfa.b1l1': False,
+    'mqxfa.a1l1': True,
+    'mqxfa.a1r1': False,
+    'mqxfa.b1r1': True,
+    'mqxfb.a2r1': True,
+    'mqxfb.b2r1': False,
+    'mqxfa.a3r1': False,
+    'mqxfa.b3r1': True,
+    'mqxfa.b3l5': False,
+    'mqxfa.a3l5': True,
+    'mqxfb.b2l5': True,
+    'mqxfb.a2l5': False,
+    'mqxfa.b1l5': False,
+    'mqxfa.a1l5': True,
+    'mqxfa.a1r5': False,
+    'mqxfa.b1r5': True,
+    'mqxfb.a2r5': True,
+    'mqxfb.b2r5': False,
+    'mqxfa.a3r5': False,
+    'mqxfa.b3r5': True,
+    'mbxf.4l1': False,
+    'mbxf.4r1': True,
+    'mbxf.4l5': False,
+    'mbxf.4r5': True,
+
+    'mbrd.4l1': True,
+    'mbrd.4r1': False,
+    'mbrd.4l5': True,
+    'mbrd.4r5': False,
+
+    # never used, to be checked
+    # 'mcbrdh.4l1': False,
+    # 'mcbrdv.4l1': True,
+    # 'mcbrdv.4r1': False,
+    # 'mcbrdh.4r1': True,
+    # 'mcbrdh.4l5': False,
+    # 'mcbrdv.4l5': True,
+    # 'mcbrdv.4r5': False,
+    # 'mcbrdh.4r5': True
+}
+
+def load_hllhc_multipole_json(fname):
+    data = xt.json.load(fname)
+
+    magnet_meas_data = {}
+    for mult in data['multipoles']:
+        aaa = mult['an']
+        bbb = mult['bn']
+        nnn = mult['n']
+        magnet_meas_data[f'a{nnn}'] = aaa
+        magnet_meas_data[f'b{nnn}'] = bbb
+
+    magnet_meas_data['ref_radius'] = data['reference_radius_mm'] * 1e-3
+
+    return magnet_meas_data
